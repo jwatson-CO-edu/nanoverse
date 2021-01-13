06f0db4ba3092f5e091bbfa51fb723a1151f1fd3
jwatson-CO-edu <james.watson-2@colorado.edu>
2021-01-12 17:03:35 -0700

    test prog 1

---
+++ tests/test01_defaults.cpp
@@ -0,0 +1,80 @@
+/*
+MAIN_TEMPLATE.cpp
+James Watson , YYYY MONTHNAME
+A ONE-LINE DESCRIPTION OF THE FILE
+
+Dependencies:
+Template Version: 2018-06-06
+*/
+
+/***** DEV PLAN *****
+[ ] TODO_i
+*/
+
+/********** Init *********************************************************************************/
+
+/***** Includes *****/
+/** Standard **/
+#include <string>
+#include <iostream>
+
+/** Special **/
+
+/** Local **/
+#include "../helpers/config.hpp"
+#include "../helpers/OGL_utils.hpp"
+
+/***** Namespaces & Aliases *****/
+using std::string;
+using std::cout;
+using std::cerr;
+using std::endl;
+
+/***** Constants & Globals *****/
+
+
+/********** <CLASS1> *****************************************************************************/
+
+
+
+/********** Utility Functions ********************************************************************/
+
+void display(){
+    // Check for errors
+    char* where = nullptr; 
+    if( ErrCheck( where ) ){
+        cout << "There were errors:" << endl << where << endl << endl;
+    }else{
+        cout << "There were no errors." << endl;
+    }
+}
+
+
+/********** MAIN *********************************************************************************/
+
+int main( int argc , char** argv ){ // Main takes the terminal command and flags that called it as arguments
+
+    // 1. Init GLUT
+    OGL_ContextConfig params = OGL_ContextConfig();
+    cout << "Calling `init_GLUT` with result:" << init_GLUT( argc, argv , params , display ) << endl;
+
+    // 2. Init OGL
+    init_OGL( params );
+
+    // the last GLUT call (LOOP)
+    // window will be shown and display callback is triggered by events
+    // NOTE: this call never return main().
+    glutMainLoop(); /* Start GLUT event-processing loop */
+
+    return 0; // I guess everything turned out alright at the end!
+}
+
+
+
+
+/********** SPARE PARTS ***************************************************************************
+
+
+
+
+*/
