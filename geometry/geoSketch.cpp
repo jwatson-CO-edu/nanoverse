b9e7f93cbb0ae4aadb1cec5294a6b57964479041
jwatson-CO-edu <james.watson-2@colorado.edu>
2021-03-09 13:19:45 -0700

    do not worry about facets until you need them

--- geometry/geoSketch.cpp
+++ geometry/geoSketch.cpp
@@ -1,7 +1,7 @@
 /***** Environment *******************************************************************************/
 
 /***** Include *****/
-#include <../glm/vec4.hpp> 
+#include <../glm/glm/vec4.hpp> 
 /*** Local ***/
 #include "../helpers/config.hpp"
 
@@ -22,20 +22,13 @@ class Mesh{ public: EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 // bool add_tri( vec3e v1, vec3e v2, vec3e v3 ); // MAYBE LATER
 // bool load_geo_from_file( string fPath ); // MAYBE LATER
 
-/* Getters */
-matXe& get_vertices();
-matXi& get_facets();
-matXe& get_normals();
-
 /*** Vars ***/
 matXe V; // Points of the mesh
-matXi F; // Facets corresponding to the points V
 matXe N; // Mesh normals
+matXe C; // Vertex Colors
 };
 
-matXe& Mesh::get_vertices(){  return V;  } // Return a reference to vertices
-matXi& Mesh::get_facets(){    return F;  } // Return a reference to facets
-matXe& Mesh::get_normals(){   return N;  } // Return a reference to normals
+
 
 
 
@@ -69,24 +62,63 @@ Cuboid::Cuboid( typeF sideX, typeF sideY, typeF sideZ ){
 // of 36 vertices (6 sides * 2 tris * 3 vertices = 36 vertices). And, each
 // vertex is 3 components (x,y,z) of floats, therefore, the size of vertex
 // array is 108 floats (36 * 3 = 108).
-V  <<  hX, hY, hZ,  -hX, hY, hZ,  -hX,-hY, hZ,  // v0-v1-v2 (front)
-      -hX,-hY, hZ,   hX,-hY, hZ,   hX, hY, hZ,  // v2-v3-v0
+V.resize( 12, 9 );
+V  <<  hX, hY, hZ,  -hX, hY, hZ,  -hX,-hY, hZ ,  // v0-v1-v2 (front)
+      -hX,-hY, hZ,   hX,-hY, hZ,   hX, hY, hZ ,  // v2-v3-v0
 
-       hX, hY, hZ,   hX,-hY, hZ,   hX,-hY,-hZ ; // v0-v3-v4 (right)
-       hX,-hY,-hZ,   hX, hY,-hZ,   hX, hY, hZ ; // v4-v5-v0
+       hX, hY, hZ,   hX,-hY, hZ,   hX,-hY,-hZ , // v0-v3-v4 (right)
+       hX,-hY,-hZ,   hX, hY,-hZ,   hX, hY, hZ , // v4-v5-v0
 
-       hX, hY, hZ,   hX, hY,-hZ,  -hX, hY,-hZ ; // v0-v5-v6 (top)
-      -hX, hY,-hZ,  -hX, hY, hZ,   hX, hY, hZ ; // v6-v1-v0
+       hX, hY, hZ,   hX, hY,-hZ,  -hX, hY,-hZ , // v0-v5-v6 (top)
+      -hX, hY,-hZ,  -hX, hY, hZ,   hX, hY, hZ , // v6-v1-v0
 
-      -hX, hY, hZ,  -hX, hY,-hZ,  -hX,-hY,-hZ ; // v1-v6-v7 (left)
-      -hX,-hY,-hZ,  -hX,-hY, hZ,  -hX, hY, hZ ; // v7-v2-v1
+      -hX, hY, hZ,  -hX, hY,-hZ,  -hX,-hY,-hZ , // v1-v6-v7 (left)
+      -hX,-hY,-hZ,  -hX,-hY, hZ,  -hX, hY, hZ , // v7-v2-v1
 
-      -hX,-hY,-hZ,   hX,-hY,-hZ,   hX,-hY, hZ ; // v7-v4-v3 (bottom)
-       hX,-hY, 1,   -hX,-hY, hZ,  -hX,-hY,-hZ ; // v3-v2-v7
+      -hX,-hY,-hZ,   hX,-hY,-hZ,   hX,-hY, hZ , // v7-v4-v3 (bottom)
+       hX,-hY, 1,   -hX,-hY, hZ,  -hX,-hY,-hZ , // v3-v2-v7
 
-       hX,-hY,-hZ,  -hX,-hY,-hZ,  -hX, hY,-hZ ; // v4-v7-v6 (back)
-      -hX, hY,-hZ,   hX, hY,-hZ,   hX,-hY,-hZ ; // v6-v5-v4
+       hX,-hY,-hZ,  -hX,-hY,-hZ,  -hX, hY,-hZ , // v4-v7-v6 (back)
+      -hX, hY,-hZ,   hX, hY,-hZ,   hX,-hY,-hZ , // v6-v5-v4
 }
+
+N.resize( 12, 9 );
+N << 0, 0, 1,   0, 0, 1,   0, 0, 1, // v0-v1-v2 (front)
+     0, 0, 1,   0, 0, 1,   0, 0, 1, // v2-v3-v0
+
+     1, 0, 0,   1, 0, 0,   1, 0, 0, // v0-v3-v4 (right)
+     1, 0, 0,   1, 0, 0,   1, 0, 0, // v4-v5-v0
+
+     0, 1, 0,   0, 1, 0,   0, 1, 0, // v0-v5-v6 (top)
+     0, 1, 0,   0, 1, 0,   0, 1, 0, // v6-v1-v0
+
+    -1, 0, 0,  -1, 0, 0,  -1, 0, 0, // v1-v6-v7 (left)
+    -1, 0, 0,  -1, 0, 0,  -1, 0, 0, // v7-v2-v1
+
+     0,-1, 0,   0,-1, 0,   0,-1, 0, // v7-v4-v3 (bottom)
+     0,-1, 0,   0,-1, 0,   0,-1, 0, // v3-v2-v7
+
+     0, 0,-1,   0, 0,-1,   0, 0,-1, // v4-v7-v6 (back)
+     0, 0,-1,   0, 0,-1,   0, 0,-1; // v6-v5-v4
+
+C.resize( 12, 9 );
+C << 1, 1, 1,   1, 1, 0,   1, 0, 0, // v0-v1-v2 (front)
+     1, 0, 0,   1, 0, 1,   1, 1, 1, // v2-v3-v0
+
+     1, 1, 1,   1, 0, 1,   0, 0, 1, // v0-v3-v4 (right)
+     0, 0, 1,   0, 1, 1,   1, 1, 1, // v4-v5-v0
+
+     1, 1, 1,   0, 1, 1,   0, 1, 0, // v0-v5-v6 (top)
+     0, 1, 0,   1, 1, 0,   1, 1, 1, // v6-v1-v0
+
+     1, 1, 0,   0, 1, 0,   0, 0, 0, // v1-v6-v7 (left)
+     0, 0, 0,   1, 0, 0,   1, 1, 0, // v7-v2-v1
+
+     0, 0, 0,   0, 0, 1,   1, 0, 1, // v7-v4-v3 (bottom)
+     1, 0, 1,   1, 0, 0,   0, 0, 0, // v3-v2-v7
+
+     0, 0, 1,   0, 0, 0,   0, 1, 0, // v4-v7-v6 (back)
+     0, 1, 0,   0, 1, 1,   0, 0, 1; // v6-v5-v4
 
 /***** Dart **************************************************************************************/
 class Dart : public Mesh { public: EIGEN_MAKE_ALIGNED_OPERATOR_NEW
@@ -112,7 +144,7 @@ Dart( typeF lenX, typeF lenY, typeF lenZ );
 namespace Model{
 
 
-/***** Mesh **************************************************************************************/
+/***** Model **************************************************************************************/
 
 class Model{ public: EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
