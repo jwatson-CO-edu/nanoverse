54bb70bf6d03ed5d5b8e15af74fedc038c9713a0
James Watson <james.watson-2@colorado.edu>
2021-03-08 22:04:23 -0700

    add GLM

--- geometry/geoSketch.cpp
+++ geometry/geoSketch.cpp
@@ -1,7 +1,7 @@
 /***** Environment *******************************************************************************/
 
 /***** Include *****/
-
+#include <../glm/vec4.hpp> 
 /*** Local ***/
 #include "../helpers/config.hpp"
 
@@ -49,6 +49,44 @@ Cuboid( typeF side ); // Cube with `side` length
 Cuboid( typeF sideX, typeF sideY, typeF sideZ );
 
 };
+
+Cuboid::Cuboid( typeF sideX, typeF sideY, typeF sideZ ){
+    typeF hX = sideX;
+    typeF hY = sideY;
+    typeF hZ = sideZ;
+    // cube ///////////////////////////////////////////////////////////////////////
+
+//    v6----- v5
+//   /|      /|
+//  v1------v0|
+//  | |     | |
+//  | |v7---|-|v4
+//  |/      |/
+//  v2------v3
+
+// vertex coords array for glDrawArrays() =====================================
+// A cube has 6 sides and each side has 2 triangles, therefore, a cube consists
+// of 36 vertices (6 sides * 2 tris * 3 vertices = 36 vertices). And, each
+// vertex is 3 components (x,y,z) of floats, therefore, the size of vertex
+// array is 108 floats (36 * 3 = 108).
+V  <<  hX, 1, 1,  -hX, 1, 1,  -hX,-1, 1,      // v0-v1-v2 (front)
+      -hX,-1, 1,   hX,-1, 1,   hX, 1, 1,      // v2-v3-v0
+
+       hX, 1, 1,   hX,-1, 1,   hX,-1,-1 ;      // v0-v3-v4 (right)
+       hX,-1,-1,   hX, 1,-1,   1, 1, 1 ;      // v4-v5-v0
+
+       hX, 1, 1,   hX, 1,-1,  -1, 1,-1 ;      // v0-v5-v6 (top)
+      -hX, 1,-1,  -hX, 1, 1,   1, 1, 1 ;      // v6-v1-v0
+
+      -hX, 1, 1,  -hX, 1,-1,  -1,-1,-1 ;      // v1-v6-v7 (left)
+      -hX,-1,-1,  -hX,-1, 1,  -1, 1, 1 ;      // v7-v2-v1
+
+      -hX,-1,-1,   hX,-1,-1,   1,-1, 1 ;      // v7-v4-v3 (bottom)
+       hX,-1, 1,  -hX,-1, 1,  -1,-1,-1 ;      // v3-v2-v7
+
+       hX,-1,-1,  -hX,-1,-1,  -1, 1,-1 ;      // v4-v7-v6 (back)
+      -hX, 1,-1,   hX, 1,-1,   1,-1,-1 ;    // v6-v5-v4
+}
 
 /***** Dart **************************************************************************************/
 class Dart : public Mesh { public: EIGEN_MAKE_ALIGNED_OPERATOR_NEW
