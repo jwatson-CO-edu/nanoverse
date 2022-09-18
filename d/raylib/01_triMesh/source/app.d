ffc398d6a359f96ddac0ddf78b74b2d20b574ab0
James Watson <james.watson-2@colorado.edu>
2022-09-18 12:03:12 -0600

    please name dub outputs "*.app" so that they will be ignored

--- d/raylib/01_triMesh/source/app.d
+++ d/raylib/01_triMesh/source/app.d
@@ -1,6 +1,7 @@
 // Adapted from: https://www.reddit.com/r/raylib/comments/v2su1s/help_with_dynamic_mesh_creation_in_raylib/
 
-import raylib;
+import core.stdc.stdlib; // `malloc`
+import raylib; // --------- easy graphics
 
 void main()
 {
@@ -23,23 +24,44 @@ void main()
     );
 
     // Entities
+    int n = 1;
     Mesh mesh = Mesh();
-    float[9] verts = [ 0.0, 0.0, 0.0 , 
-                       0.0, 0.0, 1.0 ,
-                       0.0, 1.0, 0.0 ];
+    mesh.triangleCount = n;
+    mesh.vertexCount   = n * 3;
+    mesh.vertices      = cast(float*)malloc(float.sizeof * mesh.vertexCount * 3);
+    mesh.indices       = cast(ushort*)malloc(ushort.sizeof * mesh.vertexCount);
+    
+    mesh.vertices[0]   = 0.0;
+    mesh.vertices[1]   = 0.0;
+    mesh.vertices[2]   = 0.0;
+    
+    mesh.vertices[3]   = 0.0;
+    mesh.vertices[4]   = 0.0;
+    mesh.vertices[5]   = 1.0;
+    
+    mesh.vertices[6]   = 0.0;
+    mesh.vertices[7]   = 1.0;
+    mesh.vertices[8]   = 0.0;
+    
+    mesh.indices[0]    = 0;
+    mesh.indices[1]    = 1;
+    mesh.indices[2]    = 2;
 
-    int[3]   indcs = [ 0, 1, 2 ];
+    UploadMesh(&mesh, true);
+    Model model = LoadModelFromMesh(mesh);
 
-    float* foo = new float[9];
-
-    mesh.vertices = foo;
-
-	while (!WindowShouldClose())
+    while (!WindowShouldClose())
 	{
-		// BeginDrawing();
-		// ClearBackground(Colors.RAYWHITE);
-		// DrawText("Hello, World!", 400, 300, 28, Colors.BLACK);
-		// EndDrawing();
+		BeginDrawing();
+		ClearBackground(Colors.RAYWHITE);
+
+        BeginMode3D(camera);
+        DrawModelWires(model, Vector3(0.0, 0.0, 0.0), 1.00, Colors.RED);
+        DrawGrid(10, 1.0);
+        EndMode3D();
+
+		DrawText("Hello, World!", 400, 300, 28, Colors.BLACK);
+		EndDrawing();
 	}
 	CloseWindow();
 }
