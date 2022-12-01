f25aa9c4d717c1089f587bd5cd3ee47822f284fa
James Watson <james.watson-2@colorado.edu>
2022-12-01 15:45:27 -0700

    axes always return 0

--- d/raylib/04_jsInput/source/app.d
+++ d/raylib/04_jsInput/source/app.d
@@ -18,39 +18,47 @@ void main(){
     SetTargetFPS(60);
 
 	/// Input Devices ///
-	bool         GP0_found  = IsGamepadAvailable(0);
-	bool         GP1_found  = IsGamepadAvailable(1);
-	bool         GP2_found  = IsGamepadAvailable(2);
+	bool /*---*/ GP0_found  = false;
+	bool /*---*/ GP1_found  = false;
+	bool /*---*/ GP2_found  = false;
 	const(char)* gpMsg0     = cast(char*) new char[128];
 	const(char)* gpMsg1     = cast(char*) new char[128];
 	const(char)* gpMsg2     = cast(char*) new char[128];
 
-	while (!WindowShouldClose()){
+	while ( !WindowShouldClose() ){
 
 		/// Rendering ///
-
 		BeginDrawing();
 
 		// Check Inputs // 
-		GP0_found  = IsGamepadAvailable(0);
-	    GP1_found  = IsGamepadAvailable(1);
-		GP2_found  = IsGamepadAvailable(2);
-		gpMsg0     =  toStringz( "Gamepad 0 Available?: " ~ to!string( GP0_found ) ); // Test if gamepad 0 was found
-		gpMsg1     =  toStringz( "Gamepad 1 Available?: " ~ to!string( GP1_found ) ); // Test if gamepad 1 was found
-		gpMsg2     =  toStringz( "Gamepad 2 Available?: " ~ to!string( GP2_found ) ); // Test if gamepad 2 was found
+		GP0_found = IsGamepadAvailable(0);
+	    GP1_found = IsGamepadAvailable(1);
+		GP2_found = IsGamepadAvailable(2);
+		gpMsg0    = toStringz( "Gamepad 0 Available?: " ~ to!string( GP0_found ) ); // Test if gamepad 0 was found
+		gpMsg1    = toStringz( "Gamepad 1 Available?: " ~ to!string( GP1_found ) ); // Test if gamepad 1 was found
+		gpMsg2    = toStringz( "Gamepad 2 Available?: " ~ to!string( GP2_found ) ); // Test if gamepad 2 was found
 
-		ClearBackground(Colors.RAYWHITE);
+		ClearBackground( Colors.RAYWHITE );
 
-		DrawRectangleLines(100, 200, 200, 200, Colors.BLACK);
-		DrawRectangleLines(500, 200, 200, 200, Colors.BLACK);
+		// writeln( GetGamepadAxisMovement(0, GamepadAxis.GAMEPAD_AXIS_RIGHT_X) );
+		writeln( GetGamepadAxisCount(0) ); // 6
 
-		DrawCircle(200, 300, 12.5, Colors.BLUE);
-		DrawCircle(600, 300, 12.5, Colors.BLUE);
+		// Right Stick
+		DrawRectangleLines( 100, 200, 200, 200, Colors.BLACK );
+		DrawCircle( cast(int)(200 + GetGamepadAxisMovement(0, GamepadAxis.GAMEPAD_AXIS_RIGHT_X)*200),
+                    cast(int)(300 + GetGamepadAxisMovement(0, GamepadAxis.GAMEPAD_AXIS_RIGHT_Y)*200), 
+					12.5, Colors.BLUE);
+
+		// Left Stick
+		DrawRectangleLines( 500, 200, 200, 200, Colors.BLACK );
+		DrawCircle( cast(int)(600 + GetGamepadAxisMovement(0, GamepadAxis.GAMEPAD_AXIS_LEFT_X)*200),
+                    cast(int)(300 + GetGamepadAxisMovement(0, GamepadAxis.GAMEPAD_AXIS_LEFT_Y)*200), 
+					12.5, Colors.BLUE);
 
 		
-		DrawText( gpMsg0, 25, 25, 28, Colors.BLACK);
-		DrawText( gpMsg1, 25, 50, 28, Colors.BLACK);
-		DrawText( gpMsg2, 25, 75, 28, Colors.BLACK);
+		DrawText( gpMsg0, 25, 25, 28, Colors.BLACK );
+		DrawText( gpMsg1, 25, 50, 28, Colors.BLACK );
+		DrawText( gpMsg2, 25, 75, 28, Colors.BLACK );
 
 		EndDrawing();
 
