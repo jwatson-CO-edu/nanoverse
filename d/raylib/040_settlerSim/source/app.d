11d939fbbcb9e991c3bff41785c8530a333e0046
James Watson <james.watson-2@colorado.edu>
2023-01-03 16:25:02 -0600

    basic board drawn without any pieces

--- d/raylib/040_settlerSim/source/app.d
+++ d/raylib/040_settlerSim/source/app.d
@@ -1,6 +1,7 @@
 import std.stdio;
 import std.math;
 import std.conv;
+import std.string : toStringz;
 
 import raylib; // --------- easy graphics
 import raylib.raymath; // --------- easy graphics
@@ -28,6 +29,8 @@ enum SC_Player{
 	BLU,  
 }
 
+Color[SC_Player] PLAYER_COLORS;
+
 enum SC_Resource{
 	// Resource types
 	BRCKS,
@@ -48,28 +51,59 @@ struct EdgeSC{
 	// Edge container, holds roads
 	NodeSC*[2] ends;
 	StructSC   road;
+
+	void draw(){
+		DrawLineEx( ends[0].loc, ends[1].loc, 2, Colors.BLACK); 
+	}
 }
 
 struct LandSC{
 	// Land container
-	Vector2     location; //- Location in 2D space
-	ushort /**/ yieldRoll; // Dice rolls that yields the resource
-	SC_Resource type; // ---- Type of land this is
-	Color /*-*/ color; // --- Display color
-	NodeSC*[]   nodes; // --- Nodes that this land supplies on a yield
+	Vector2 /*-*/ location; //- Location in 2D space
+	ushort /*--*/ yieldRoll; // Dice rolls that yields the resource
+	SC_Resource   type; // ---- Type of land this is
+	Color /*---*/ color; // --- Display color
+	NodeSC*[]     nodes; // --- Nodes that this land supplies on a yield
+	ushort /*--*/ id; // ------ ID for this particular node 
+	static ushort ID = 0; // -- Total number of IDs created
+
+	this( Vector2 loc, ushort roll, SC_Resource typ, Color clr ){
+		// Create the node, assign an ID, then increment created IDs
+		location  = loc; //- Location in 2D space
+		yieldRoll = roll; // Dice rolls that yields the resource
+		type /**/ = typ; //- Type of land this is
+		color     = clr; //- Display color
+		id /*--*/ = ID;
+		ID++;
+	}
 
 	void draw(){
 		DrawPoly( location, 6, EDGE_LEN, 0.0, color);
+		DrawText( (yieldRoll.to!string).toStringz(), cast(int) location.x, cast(int) location.y, 40, Colors.BLACK );
 	}
 }
 
 struct NodeSC{
 	// Board vertex container
-	Vector2    loc; // Location in 2D space
-	bool /*-*/ opn; // Is this node open for building?
-	StructSC   con; // Structure contents
-	GreyManSC* vst; // Pointer to The Grey Man, when present
-	EdgeSC*[]  edg; // Edges connected to node
+	Vector2 /*-*/ loc; // -- Location in 2D space
+	bool /*----*/ opn; // -- Is this node open for building?
+	StructSC /**/ con; // -- Structure contents
+	GreyManSC*    vst; // -- Pointer to The Grey Man, when present
+	EdgeSC*[]     edg; // -- Edges connected to node
+	ushort /*--*/ id; // --- ID for this particular node 
+	static ushort ID = 0; // Total number of IDs created
+	
+	this( Vector2 location ){
+		// Create the node, assign an ID, then increment created IDs
+		loc = location;
+		opn = true;
+		id  = ID;
+		ID++;
+	}
+
+	void draw(){
+		DrawCircle( cast(int) loc.x, cast(int) loc.y, EDGE_LEN/8.0, Colors.BLACK);
+	}
 }
 
 class GreyManSC{
@@ -99,10 +133,11 @@ struct BoardSC{
 	// Container for everything on the game board
 
 	/// Members ///
-	SC_Resource[] bank;  // --------- Total unowned resources
-	LandSC*[]     hexes; // --------- Land tiles
-	NodeSC*[]     nodes; // --------- Land vertices
-	EdgeSC*[]     edges; // --------- Segments between nodes, possibly holding roads
+	SC_Resource[]   bank;  // --------- Total unowned resources
+	LandSC*[] /*-*/ hexes; // --------- Land tiles
+	LandSC*[ushort] hexLookup; // ----- Find a hex by its ID
+	NodeSC*[] /*-*/ nodes; // --------- Land vertices
+	EdgeSC*[] /*-*/ edges; // --------- Segments between nodes, possibly holding roads
 
 	/// Methods ///
 	
@@ -117,8 +152,9 @@ struct BoardSC{
 		// If the (4-player) board is not full, then proceed with adding a tile
 		if( hexes.length < TILE_CENTERS.length ){
 
-			// Add the tile
+			// Add the tile to the board and the lookup
 			hexes ~= nuTile;
+			hexLookup[ nuTile.id ] = nuTile; 
 
 			// For each of the possible node locations
 			foreach( Vector2 rel; REL_NODE_LOCS ){
@@ -127,37 +163,44 @@ struct BoardSC{
 
 				// Get the absolute node location
 				searchLoc = Vector2Add( nuTile.location, rel );
+				writeln( "Find or create a node at " ~ searchLoc.to!string ~ " = " ~ 
+				 		 nuTile.location.to!string ~ " + " ~ rel.to!string );
 				
 				// Search for an existing node
 				foreach( NodeSC* node; nodes ){
 					// Check the distance between the prospective node and the existing node
-					if( Vector2DistanceSqr( node.loc, searchLoc ) < (EDGE_LEN/4.0f) ){
+					// if( Vector2DistanceSqr( node.loc, searchLoc ) < (EDGE_LEN/4.0f) ){
+					if( Vector2DistanceSqr( node.loc, searchLoc ) < (EDGE_LEN) ){
 						// If node already exists, then add a pointer to it
 						nuTile.nodes ~= node;
-						nodes ~= nuNode; // Add to node collection
 						foundNode = true;
+						writeln( "Found a MATCH at " ~ node.loc.to!string );
 						break;
 					}
 				}
 				// If node DNE, then create and set pointer to it
 				if( !foundNode ){
-					nuNode = new NodeSC( searchLoc, true );
+					nuNode = new NodeSC( searchLoc );
 					nuTile.nodes ~= nuNode; // Add to tile nodes
 					nodes ~= nuNode; // Add to node collection
 				}
 			}
 
 			// For each of the nodes attached to the tile
-			ushort i = 0;
+			// ushort i = 0;
 			lastNode = nuTile.nodes[$-1];
 			foreach( NodeSC* node; nuTile.nodes ){ // Assume that the nodes were added in clockwise order, above
+				writeln( "Edge [ " ~ lastNode.id.to!string ~ ", " ~ node.id.to!string ~ " ]"  );
 				foundNode = false;
 				// Check that there isn't already a connection between this node and last
-				foreach( EdgeSC* edge; node.edg ){
-					if( ((edge.ends[0] == lastNode) && (edge.ends[1] == node)) 
+				// foreach( EdgeSC* edge; node.edg ){
+				foreach( EdgeSC* edge; edges ){
+					writeln( "Compare [ " ~ edge.ends[0].id.to!string ~ ", " ~ edge.ends[1].id.to!string ~ " ]"  );					
+					if( ((edge.ends[0].id == lastNode.id) && (edge.ends[1].id == node.id)) 
 						|| 
-						((edge.ends[1] == lastNode) && (edge.ends[0] == node)) ){
+						((edge.ends[1].id == lastNode.id) && (edge.ends[0].id == node.id)) ){
 						foundNode = true;
+						writeln( "EDGE MATCH" );
 						break;
 					}
 				}
@@ -177,24 +220,37 @@ struct BoardSC{
 	}
 
 	void draw_tiles(){
-		foreach( LandSC* tile, hexes ){
-			
+		// Draw all the tiles on the map
+		foreach( LandSC* tile; hexes ){
+			tile.draw();
 		}
 	}
 
 	void draw_nodes(){
-
+		// Draw all the nodes and structures at nodes
+		foreach( NodeSC* node; nodes ){
+			node.draw();
+		}
 	}
 
 	void draw_edges(){
+		// Draw all the edges and the roads at edges
+		foreach( EdgeSC* edge; edges ){
+			edge.draw();
+		}
+	}
 
+	void draw_state(){
+		draw_tiles();
+		draw_nodes();
+		draw_edges();
 	}
 }
 
 
 void main(){
 
-	///// Setup //////////////////////////////////
+	///// Game Constants /////////////////////////
 	
 	TILE_CENTERS = [
 		
@@ -246,66 +302,191 @@ void main(){
 		// Row 4
 		Vector2( 
 			400 - EDGE_LEN*cos(pi/6.0), 
-			200 + 8*EDGE_LEN*cos(pi/3.0) 
+			200 + 9*EDGE_LEN*cos(pi/3.0) 
 		),
 		Vector2( 
 			400 - EDGE_LEN*cos(pi/6.0) + 2*EDGE_LEN*cos(pi/6.0), 
-			200 + 8*EDGE_LEN*cos(pi/3.0) 
+			200 + 9*EDGE_LEN*cos(pi/3.0) 
 		),
 		Vector2( 
 			400 - EDGE_LEN*cos(pi/6.0) + 4*EDGE_LEN*cos(pi/6.0), 
-			200 + 8*EDGE_LEN*cos(pi/3.0) 
+			200 + 9*EDGE_LEN*cos(pi/3.0) 
 		),
 		Vector2( 
 			400 - EDGE_LEN*cos(pi/6.0) + 6*EDGE_LEN*cos(pi/6.0), 
-			200 + 8*EDGE_LEN*cos(pi/3.0) 
+			200 + 9*EDGE_LEN*cos(pi/3.0) 
 		),
 
 		// Row 5
 		Vector2( 
 			400, 
-			200 + 10*EDGE_LEN*cos(pi/3.0) 
+			200 + 12*EDGE_LEN*cos(pi/3.0) 
 		),
 		Vector2( 
 			400 + 2*EDGE_LEN*cos(pi/6.0), 
-			200 + 10*EDGE_LEN*cos(pi/3.0) 
+			200 + 12*EDGE_LEN*cos(pi/3.0) 
 		),
 		Vector2( 
 			400 + 4*EDGE_LEN*cos(pi/6.0), 
-			200 + 10*EDGE_LEN*cos(pi/3.0) 
+			200 + 12*EDGE_LEN*cos(pi/3.0) 
 		),
 	];
-
-	BoardSC board = BoardSC(); 
-
-	// LandSC l1 = LandSC(
-	board.add_tile( new LandSC(
-		TILE_CENTERS[0], //- Location in 2D space
-		6, // Dice rolls that yields the resource
-		SC_Resource.ROCKS, // ---- Type of land this is
-		Colors.GRAY // --- Display color
-	) );
-
-	writeln( "There are " ~ board.hexes.length.to!string ~ " tiles." );
-	writeln( "There are " ~ board.nodes.length.to!string ~ " nodes." );
-	writeln( "There are " ~ board.edges.length.to!string ~ " edges." );
-
-	LandSC l2 = LandSC(
-		TILE_CENTERS[3], //- Location in 2D space
-		6, // Dice rolls that yields the resource
-		SC_Resource.SHEEP, // ---- Type of land this is
-		Colors.GREEN // --- Display color
-	);
 
 	REL_NODE_LOCS = [ // Locations of nodes relative to a land tile center
 		Vector2( 0.0f, EDGE_LEN ),
 		Vector2Rotate( Vector2( 0.0f, EDGE_LEN ), -1.0f*pi/3.0f ),
-		// Vector2( 0.0f, EDGE_LEN ).Rotate(  -1.0*PI/3.0 ),
 		Vector2Rotate( Vector2( 0.0f, EDGE_LEN ), -2.0*pi/3.0 ),
 		Vector2Rotate( Vector2( 0.0f, EDGE_LEN ), -3.0*pi/3.0 ),
 		Vector2Rotate( Vector2( 0.0f, EDGE_LEN ), -4.0*pi/3.0 ),
 		Vector2Rotate( Vector2( 0.0f, EDGE_LEN ), -5.0*pi/3.0 ),
 	];
+
+	PLAYER_COLORS = [ // Colors for rendering player properties
+		SC_Player.RED : Colors.RED   ,
+		SC_Player.ORN : Colors.ORANGE,
+		SC_Player.WHT : Colors.WHITE ,
+		SC_Player.BLU : Colors.BLUE  ,
+	];
+
+	///// Board Setup ////////////////////////////
+
+	BoardSC board = BoardSC(); 
+
+	/// Lay Down Tiles ///
+
+	board.add_tile( new LandSC(
+		TILE_CENTERS[0], //- Location in 2D space
+		10, // Dice rolls that yields the resource
+		SC_Resource.ROCKS, // ---- Type of land this is
+		Colors.GRAY // --- Display color
+	) );
+
+	board.add_tile( new LandSC(
+		TILE_CENTERS[1], //- Location in 2D space
+		2, // Dice rolls that yields the resource
+		SC_Resource.SHEEP, // ---- Type of land this is
+		Colors.GREEN // --- Display color
+	) );
+
+	board.add_tile( new LandSC(
+		TILE_CENTERS[2], //- Location in 2D space
+		9, // Dice rolls that yields the resource
+		SC_Resource.LUMBR, // ---- Type of land this is
+		Colors.DARKGREEN // --- Display color
+	) );
+
+	board.add_tile( new LandSC(
+		TILE_CENTERS[3], //- Location in 2D space
+		12, // Dice rolls that yields the resource
+		SC_Resource.GRAIN, // ---- Type of land this is
+		Colors.GOLD // --- Display color
+	) );
+
+	board.add_tile( new LandSC(
+		TILE_CENTERS[4], //- Location in 2D space
+		6, // Dice rolls that yields the resource
+		SC_Resource.BRCKS, // ---- Type of land this is
+		Colors.MAROON // --- Display color
+	) );
+
+	board.add_tile( new LandSC(
+		TILE_CENTERS[5], //- Location in 2D space
+		4, // Dice rolls that yields the resource
+		SC_Resource.SHEEP, // ---- Type of land this is
+		Colors.GREEN // --- Display color
+	) );
+
+	board.add_tile( new LandSC(
+		TILE_CENTERS[6], //- Location in 2D space
+		10, // Dice rolls that yields the resource
+		SC_Resource.BRCKS, // ---- Type of land this is
+		Colors.MAROON // --- Display color
+	) );
+
+	board.add_tile( new LandSC(
+		TILE_CENTERS[7], //- Location in 2D space
+		9, // Dice rolls that yields the resource
+		SC_Resource.GRAIN, // ---- Type of land this is
+		Colors.GOLD // --- Display color
+	) );
+
+	board.add_tile( new LandSC(
+		TILE_CENTERS[8], //- Location in 2D space
+		11, // Dice rolls that yields the resource
+		SC_Resource.LUMBR, // ---- Type of land this is
+		Colors.DARKGREEN // --- Display color
+	) );
+
+	board.add_tile( new LandSC(
+		TILE_CENTERS[9], //- Location in 2D space
+		7, // Dice rolls that yields the resource
+		SC_Resource.WLDCD, // ---- Type of land this is
+		Colors.BEIGE // --- Display color
+	) );
+
+	board.add_tile( new LandSC(
+		TILE_CENTERS[10], //- Location in 2D space
+		3, // Dice rolls that yields the resource
+		SC_Resource.LUMBR, // ---- Type of land this is
+		Colors.DARKGREEN // --- Display color
+	) );
+
+	board.add_tile( new LandSC(
+		TILE_CENTERS[11], //- Location in 2D space
+		8, // Dice rolls that yields the resource
+		SC_Resource.ROCKS, // ---- Type of land this is
+		Colors.GRAY // --- Display color
+	) );
+
+	board.add_tile( new LandSC(
+		TILE_CENTERS[12], //- Location in 2D space
+		8, // Dice rolls that yields the resource
+		SC_Resource.LUMBR, // ---- Type of land this is
+		Colors.DARKGREEN // --- Display color
+	) );
+
+	board.add_tile( new LandSC(
+		TILE_CENTERS[13], //- Location in 2D space
+		3, // Dice rolls that yields the resource
+		SC_Resource.ROCKS, // ---- Type of land this is
+		Colors.GRAY // --- Display color
+	) );
+
+	board.add_tile( new LandSC(
+		TILE_CENTERS[14], //- Location in 2D space
+		4, // Dice rolls that yields the resource
+		SC_Resource.GRAIN, // ---- Type of land this is
+		Colors.GOLD // --- Display color
+	) );
+
+	board.add_tile( new LandSC(
+		TILE_CENTERS[15], //- Location in 2D space
+		5, // Dice rolls that yields the resource
+		SC_Resource.SHEEP, // ---- Type of land this is
+		Colors.GREEN // --- Display color
+	) );
+
+	board.add_tile( new LandSC(
+		TILE_CENTERS[16], //- Location in 2D space
+		5, // Dice rolls that yields the resource
+		SC_Resource.BRCKS, // ---- Type of land this is
+		Colors.MAROON // --- Display color
+	) );
+
+	board.add_tile( new LandSC(
+		TILE_CENTERS[17], //- Location in 2D space
+		6, // Dice rolls that yields the resource
+		SC_Resource.GRAIN, // ---- Type of land this is
+		Colors.GOLD // --- Display color
+	) );
+
+	board.add_tile( new LandSC(
+		TILE_CENTERS[18], //- Location in 2D space
+		11, // Dice rolls that yields the resource
+		SC_Resource.SHEEP, // ---- Type of land this is
+		Colors.GREEN // --- Display color
+	) );
+
 
 	///// Rendering //////////////////////////////
 
@@ -313,10 +494,14 @@ void main(){
 	validateRaylibBinding();
 
 	// Window / Display Params
-    InitWindow(1200, 900, "Hello, Raylib-D!");
+    InitWindow(1200, 900, "Settlers of C*t*n Simulation");
     SetTargetFPS(60);
 
 	writeln( "##### Settlers of C*t*n Simulation, Version 2022-12 #####" );
+	writeln( "There are " ~ board.hexes.length.to!string ~ " tiles." );
+	writeln( "There are " ~ board.nodes.length.to!string ~ " nodes." );
+	writeln( "There are " ~ board.edges.length.to!string ~ " edges." );
+	// writeln( board.nodes[0] == board.nodes[0] );
 
 
 	while ( !WindowShouldClose() ){
@@ -328,7 +513,11 @@ void main(){
 
 		// l1.draw();
 		// l2.draw();
-		
+		// board.draw_tiles();
+		// board.draw_nodes();
+		// board.draw_edges();
+		board.draw_state();
+
 		EndDrawing();
 
 	}
