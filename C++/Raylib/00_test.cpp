9a9402d6f8a13885d320e34d9fdc6f81fb6c8ef0
James Watson <james.watson-2@colorado.edu>
2023-03-30 13:32:02 -0600

    Raylib C++ to jog memory

---
+++ C++/Raylib/00_test.cpp
@@ -0,0 +1,19 @@
+// https://github.com/raysan5/raylib#basic-example
+
+#include "raylib.h"
+
+int main( void ){
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground( RAYWHITE );
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
