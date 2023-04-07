818b54d0d5bfee660ec03148dbd0098d16d882e9
James Watson <james.watson-2@colorado.edu>
2023-04-07 10:01:55 -0600

    Transferring SDL2 event polling code from Lazy Foo

--- C++/Raylib/08_SDL-JS-detect.cpp
+++ C++/Raylib/08_SDL-JS-detect.cpp
@@ -5,9 +5,16 @@ g++ 08_SDL-JS-detect.cpp -lSDL2main -lSDL2
 https://lazyfoo.net/tutorials/SDL/19_gamepads_and_joysticks/index.php
 */
 
+////////// INIT ////////////////////////////////////////////////////////////////////////////////////
+
 #include <iostream>
 using std::cout, std::endl;
 #include <SDL2/SDL.h> 
+
+///// Globals ////////////////////////////////////
+
+//Event handler
+SDL_Event e;
 
 ////////// JOYSTICK @ SDL2 /////////////////////////////////////////////////////////////////////////
 
@@ -35,6 +42,78 @@ void close_js_SDL2( SDL_Joystick*& jsPtr ){
     cout << "Connection to joystick closed!" << endl;
 }
 
+///// Input Struct ////////////////////////////////////
+
+struct InputState{
+    // Container struct for input state
+
+    // Thumb Sticks
+    float GAMEPAD_AXIS_RIGHT_X;
+    float GAMEPAD_AXIS_RIGHT_Y;
+    float GAMEPAD_AXIS_LEFT_X;
+    float GAMEPAD_AXIS_LEFT_Y;
+    
+    // Face buttons
+    bool GAMEPAD_BUTTON_RIGHT_FACE_UP;
+    bool GAMEPAD_BUTTON_RIGHT_FACE_LEFT;
+    bool GAMEPAD_BUTTON_RIGHT_FACE_RIGHT;
+    bool GAMEPAD_BUTTON_RIGHT_FACE_DOWN;
+
+    // D-pad
+    bool GAMEPAD_BUTTON_LEFT_FACE_UP;
+    bool GAMEPAD_BUTTON_LEFT_FACE_LEFT;
+    bool GAMEPAD_BUTTON_LEFT_FACE_RIGHT;
+    bool GAMEPAD_BUTTON_LEFT_FACE_DOWN;
+};
+
+void poll_SDL2_inputs( InputState& state ){
+    // Populate the `state` based on the SDL2 input queue
+
+    //Analog joystick dead zone
+    const int JOYSTICK_DEAD_ZONE = 8000;
+
+    //Normalized direction
+    int xDir = 0;
+    int yDir = 0;
+
+    //Handle events on queue
+    while( SDL_PollEvent( &e ) != 0 ){
+        // if Motion on controller 0
+        if( (e.type == SDL_JOYAXISMOTION) && (e.jaxis.which == 0) ){
+            
+            //X axis motion
+            if( e.jaxis.axis == 0 ){
+                //Left of dead zone
+                if( e.jaxis.value < -JOYSTICK_DEAD_ZONE ){
+                    xDir = -1;
+                }
+                //Right of dead zone
+                else if( e.jaxis.value > JOYSTICK_DEAD_ZONE ){
+                    xDir = 1;
+                }else{
+                    xDir = 0;
+                }
+            }
+            //Y axis motion
+            else if( e.jaxis.axis == 1 ){
+                //Below of dead zone
+                if( e.jaxis.value < -JOYSTICK_DEAD_ZONE ){
+                    yDir = -1;
+                }
+                //Above of dead zone
+                else if( e.jaxis.value > JOYSTICK_DEAD_ZONE ){
+                    yDir =  1;
+                }else{
+                    yDir = 0;
+                }
+            }
+        }
+    }
+
+    
+}
+
+
 ////////// MAIN ////////////////////////////////////////////////////////////////////////////////////
 
 int main(){
@@ -46,7 +125,11 @@ int main(){
     
     js0ptr = init_js0_SDL2();
 
-    // FIXME, START HERE: ADD EVENT LOOP FROM https://lazyfoo.net/tutorials/SDL/19_gamepads_and_joysticks/index.php
+    // https://lazyfoo.net/tutorials/SDL/19_gamepads_and_joysticks/index.php
+     //Main loop flag
+    
+
+    
 
     //Close game controller
     close_js_SDL2( js0ptr );
