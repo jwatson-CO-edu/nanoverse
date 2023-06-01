aad7320a9d232eecedf9ca0a6cdb5bae79b1566b
James Watson <james.watson-2@colorado.edu>
2023-06-01 11:36:36 -0600

    `HoopTarget` test harness, need to show drawing hoop in idle and scored states

--- C++/Raylib/12-3_stateful-hoop.cpp
+++ C++/Raylib/12-3_stateful-hoop.cpp
@@ -10,6 +10,10 @@ using std::cout, std::endl, std::ostream;
 using std::min, std::max;
 #include <deque>
 using std::deque;
+#include <array>
+using std::array;
+#include <memory>
+using std::shared_ptr;
 
 /// Raylib ///
 #include <raylib.h>
@@ -31,7 +35,7 @@ typedef unsigned short  ushort;
 ////////// CLASSES /////////////////////////////////////////////////////////////////////////////////
 
 class HoopTarget{ public:
-    // A hoop for the player to fly through
+    // A hoop for the player to fly through, Reminiscent of an Identity Disk from Tron
 
     /// Members ///
     Vector3 center;
@@ -58,6 +62,30 @@ class HoopTarget{ public:
         clrSolidInit.a = 125;
     }
 
+    void test_segment( const Vector3& Q, const Vector3& R ){
+        // Test if a line segment QR passes thru the hoop, If so then set `scored` to true
+        // Author: saxbophone, https://math.stackexchange.com/a/4657621
+        bool    win   = false;
+        Vector3 RmQ   = Vector3Subtract( R, Q );
+        float   QRlen = Vector3Length( RmQ );
+        float   num   = Vector3DotProduct( RmQ, Vector3Subtract( Q, center ) );
+        float   den   = Vector3DotProduct( RmQ, RmQ );
+        float   tHt, dotQR;
+        Vector3 G, GmQ;
+        // Test for nonzero segment length
+        if( den > 0.0f ){
+            tHt = num / den;
+            // G is closest to the hoop center on QR, BUT may NOT be between Q and R!
+            G     = Vector3Subtract( Q, Vector3Scale( RmQ, tHt ) );
+            GmQ   = Vector3Subtract( G, Q );
+            dotQR = Vector3DotProduct( GmQ, RmQ );
+            // If dot prod negative, then center is entirely "behind" segment, otherwise test within segment
+            if( dotQR > 0.0f ){  win = (Vector3Length( GmQ ) < QRlen);  }
+        }
+        // If we won, then latch in the `scored` state, Let the client code unlatch if needed
+        if( win )  scored = true;
+    }
+
     void draw(){
         // Render hoop as a batch job
 
@@ -68,7 +96,7 @@ class HoopTarget{ public:
         Vector3 bgnRay = radRay;
         float   incr   = 2.0f * PI / (1.0f * Nseg);
         float   margin = 0.02 * radOuter;
-        Vector3 p1, p2, p3, p4;
+        Vector3 p1, p2, p3, p4, p5, p6;
         p1 = Vector3Add( center, Vector3Scale( radRay, radInner ) );
         p2 = Vector3Add( center, Vector3Scale( radRay, radOuter ) );
 
@@ -105,6 +133,7 @@ class HoopTarget{ public:
         radRay = bgnRay;
         p1 = Vector3Add( center, Vector3Scale( radRay, radInner-margin ) );
         p2 = Vector3Add( center, Vector3Scale( radRay, radOuter+margin ) );
+        p5 = Vector3Add( center, Vector3Scale( radRay, radOuter+2.0*margin ) );
         
         rlBegin( RL_LINES );
 
@@ -115,6 +144,7 @@ class HoopTarget{ public:
             radRay = Vector3RotateByAxisAngle( radRay, normal, incr );
             p3     = Vector3Add( center, Vector3Scale( radRay, radInner-margin ) );
             p4     = Vector3Add( center, Vector3Scale( radRay, radOuter+margin ) );
+            p6     = Vector3Add( center, Vector3Scale( radRay, radOuter+2.0*margin ) );
 
             /// Segment 1: p1-to-p3 ///
             rlVertex3f( p1.x, p1.y, p1.z );
@@ -124,13 +154,48 @@ class HoopTarget{ public:
             rlVertex3f( p2.x, p2.y, p2.z );
             rlVertex3f( p4.x, p4.y, p4.z );
 
+            /// Segment 3: p5-to-p6 ///
+            rlVertex3f( p5.x, p5.y, p5.z );
+            rlVertex3f( p6.x, p6.y, p6.z );
+
             p1 = p3;
             p2 = p4;
+            p5 = p6;
         }
 
         // End border batch job
         rlEnd();
     }
+};
+
+class HoopTester{ public:
+    // Test harness for `HoopTarget`
+    Vector3 /*----------*/ minC;
+    Vector3 /*----------*/ maxC;
+    shared_ptr<HoopTarget> hoop;
+
+    HoopTester( const Vector3& minCorner, const Vector3& maxCorner, HoopTarget* target ){
+        // Set the test bounds the the hoop to be tested
+        minC = minCorner;
+        maxC = maxCorner;
+        hoop = shared_ptr<HoopTarget>( target );
+    }
+
+    Vector3 sample_point(){
+        // Sample within the bounding box
+        return Vector3{
+            randf( minC.x, maxC.x ),
+            randf( minC.y, maxC.y ),
+            randf( minC.z, maxC.z ),
+        };
+    }
+
+    array<Vector3,2> sample_segment(){
+        // Return a line segment that begins and ends within the specfied bounds
+        return array<Vector3,2>{ sample_point(), sample_point() };
+    }
+    
+    // FIXME, START HERE: ALTERNATE TESTING STATE AND IDLE STATE
 };
 
 
@@ -148,7 +213,7 @@ int main(){
 
     // Camera
     Camera camera = Camera{
-        Vector3{ 15.0, 15.0, 50.0 }, // Position
+        Vector3{ 15.0, 30.0, 50.0 }, // Position
         Vector3{ 15.0, 15.0,  0.0 }, // Target
         Vector3{  0.0,  1.0,  0.0 }, // Up
         45.0, // ---------------------- FOV_y
