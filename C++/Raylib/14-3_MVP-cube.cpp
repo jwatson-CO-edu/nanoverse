2994cc8c7de7e50abff43ba50978cc91e5332eb3
James Watson <james.watson-2@colorado.edu>
2023-07-05 14:20:50 -0600

    haha but I did it anyway!

---
+++ C++/Raylib/14-3_MVP-cube.cpp
@@ -0,0 +1,34 @@
+// g++ 14-3_MVP-cube.cpp -std=c++17 -lraylib
+
+////////// INIT ////////////////////////////////////////////////////////////////////////////////////
+
+///// Imports ////////////////////////////////////
+
+/// Standard ///
+#include <deque>
+using std::deque;
+#include <array>
+using std::array;
+
+/// Raylib ///
+#include "raylib.h"
+#include "raymath.h"
+
+////////// TOYS ////////////////////////////////////////////////////////////////////////////////////
+
+struct DynaCube{
+    // A simple cube with optionally-drawn facets
+
+    /// Members ///
+    deque<array<Vector3,3>> triangles; // - Dynamic geometry
+    deque<array<Color,3>>   facetColors; // Vertex colors for each facet
+    deque<bool> /*-------*/ activeTri; // - Flags for whether to draw each triangle
+    Mesh /*--------------*/ mesh; // ------ Raylib mesh geometry
+	Model /*-------------*/ model; // ----- Raylib drawable model
+
+    /// Constructor ///
+    DynaCube( float sideLen ){
+        // Create triangles that make up the cube
+        float halfLen = sideLen/2.0;
+    }
+};
