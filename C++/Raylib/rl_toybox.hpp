d553d4d8a4263291d59967b09f0d9b627eef7471
James Watson <james.watson-2@colorado.edu>
2023-09-02 19:30:24 -0600

    Moved proven structs to the toybox

---
+++ C++/Raylib/rl_toybox.hpp
@@ -0,0 +1,383 @@
+#ifndef RL_TOYBOX_H
+#define RL_TOYBOX_H
+
+////////// INIT ////////////////////////////////////////////////////////////////////////////////////
+
+///// Includes ///////////////////////////////////
+
+/// Standard ///
+#include <array>
+using std::array;
+#include <vector>
+using std::vector;
+#include <algorithm>
+using std::clamp, std::min;
+#include <memory>
+using std::shared_ptr;
+
+/// Raylib ///
+#include <raylib.h>
+#include <raymath.h>
+#include <rlgl.h>
+
+/// Local ///
+#include "utils.hpp"
+
+///// Aliases ////////////////////////////////////
+typedef array<Vector3,3> triPnts; // Vector info for One Triangle (Vertices,nrms) 
+typedef array<Color,3>   triClrs; // Color  info for One Triangle
+typedef vector<Vector3>  vvec3; // - Vector of 3D vectors
+#define VERTEX_BUFFER_IDX 0 // Vertex coord VBO
+#define NORMAL_BUFFER_IDX 2 // Normal vector VBO
+#define COLORS_BUFFER_IDX 3 // Vertex color VBO
+#define INDEXF_BUFFER_IDX 6 // Indices of facet vertices VBO
+
+////////// VECTOR OPERATIONS ///////////////////////////////////////////////////////////////////////
+
+
+Vector3 normal_of_tiangle( const triPnts& tri ){
+    // Get the normal of the triangle assuming CCW / Right-Hand ordering
+    return Vector3Normalize( Vector3CrossProduct(
+        Vector3Subtract( tri[2], tri[1] ),
+        Vector3Subtract( tri[0], tri[1] )
+    ));
+}
+
+Vector3 uniform_vector_noise( const Vector3& vec, float halfMag ){
+    // Return a perturbed `vec` moved up to `halfMag` in each direction in each dimension
+    return Vector3{
+        randf( vec.x - halfMag, vec.x + halfMag ),
+        randf( vec.y - halfMag, vec.y + halfMag ),
+        randf( vec.z - halfMag, vec.z + halfMag )
+    };
+}
+
+Vector3 uniform_vector_noise( float halfMag ){
+    // Return a perturbed `vec` moved up to `halfMag` in each direction in each dimension
+    return Vector3{
+        randf( -halfMag, halfMag ),
+        randf( -halfMag, halfMag ),
+        randf( -halfMag, halfMag )
+    };
+}
+
+Color uniform_random_color(){
+    // Return a random fully opaque color
+    return Color{ rand_ubyte(), rand_ubyte(), rand_ubyte(), 255 };
+}
+
+
+
+////////// VECTOR MATH STRUCTS /////////////////////////////////////////////////////////////////////
+
+struct Basis{
+    // Pose exchange format for `Boid`s, Z-basis has primacy
+    // NOTE: None of the operations with other bases assume any operand is orthonormalized
+    // NOTE: Position is largely absent from Basis-Basis operations
+
+    /// Members ///
+    Vector3 Xb; // X-basis
+    Vector3 Yb; // Y-basis
+    Vector3 Zb; // Z-basis
+    Vector3 Pt; // Position
+
+    /// Static Methods ///
+
+    static Basis origin(){
+        // Get the origin `Basis`
+        Basis rtnBasis;
+        rtnBasis.Xb = Vector3{ 1.0f, 0.0f, 0.0f };
+        rtnBasis.Yb = Vector3{ 0.0f, 1.0f, 0.0f };
+        rtnBasis.Zb = Vector3{ 0.0f, 0.0f, 1.0f };
+        rtnBasis.Pt = Vector3{ 0.0f, 0.0f, 0.0f };
+        return rtnBasis;
+    }
+
+    static Basis random(){
+        // Sample from a +/-1.0f cube for all vectors, then `orthonormalize`
+        Basis rtnBasis;
+        rtnBasis.Xb = Vector3{ randf( -1.0,  1.0 ), randf( -1.0,  1.0 ), randf( -1.0,  1.0 ) };
+        rtnBasis.Yb = Vector3{ randf( -1.0,  1.0 ), randf( -1.0,  1.0 ), randf( -1.0,  1.0 ) };
+        rtnBasis.Zb = Vector3{ randf( -1.0,  1.0 ), randf( -1.0,  1.0 ), randf( -1.0,  1.0 ) };
+        rtnBasis.Pt = Vector3{ randf( -1.0,  1.0 ), randf( -1.0,  1.0 ), randf( -1.0,  1.0 ) };
+        rtnBasis.orthonormalize();
+        return rtnBasis;
+    }
+
+    static Basis from_transform_and_point( const Matrix& xform, const Vector3& point ){
+        // Get a `Basis` from a `xform` matrix and a `point`
+        Basis rtnBasis;
+        rtnBasis.Xb = Vector3Transform( Vector3{1.0, 0.0, 0.0}, xform );
+        rtnBasis.Yb = Vector3Transform( Vector3{0.0, 1.0, 0.0}, xform );
+        rtnBasis.Zb = Vector3Transform( Vector3{0.0, 0.0, 1.0}, xform );
+        rtnBasis.Pt = point;
+        return rtnBasis;
+    }
+
+    /// Methods ///
+
+    void orthonormalize(){
+        // Make sure this is an orthonormal basis, Z-basis has primacy
+        // No need to normalize `Xb`, see below
+        Yb = Vector3Normalize( Yb );
+        Zb = Vector3Normalize( Zb );
+        Xb = Vector3Normalize( Vector3CrossProduct( Yb, Zb ) );
+        Yb = Vector3Normalize( Vector3CrossProduct( Zb, Xb ) );
+    };
+
+    void blend_orientations_with_factor( const Basis& other, float factor ){
+        // Exponential filter between this basis and another Orthonormalize separately
+        // 1. Clamp factor
+        factor = clamp( factor, 0.0f, 1.0f );
+        // 2. Blend bases, No need to compute `Xb`, see `orthonormalize`
+        Yb = Vector3Add(  Vector3Scale( Yb, 1.0-factor ), Vector3Scale( other.Yb, factor )  );
+        Zb = Vector3Add(  Vector3Scale( Zb, 1.0-factor ), Vector3Scale( other.Zb, factor )  );
+        // 3. Correct basis
+        orthonormalize();
+    }
+
+    Basis operator+( const Basis& other ){
+        // Addition operator for bases, Just the vector sum of each basis, Orthonormalize separately
+        Basis rtnBasis;
+        rtnBasis.Xb = Vector3Add( Xb, other.Xb );
+        rtnBasis.Yb = Vector3Add( Yb, other.Yb );
+        rtnBasis.Zb = Vector3Add( Zb, other.Zb );
+        rtnBasis.Pt = Vector3Add( Pt, other.Pt );
+        return rtnBasis;
+    }
+
+    Basis get_scaled_orientation( float factor ){
+        // Return a copy of the `Basis` with each individual basis scaled by a factor
+        Basis rtnBasis;
+        rtnBasis.Xb = Vector3Scale( Xb, factor );
+        rtnBasis.Yb = Vector3Scale( Yb, factor );
+        rtnBasis.Zb = Vector3Scale( Zb, factor );
+        // rtnBasis.Pt = Vector3Scale( Pt, factor );
+        return rtnBasis;
+    }
+
+    Basis copy(){
+        // Return a copy of the basis.
+        Basis rtnBasis;
+        rtnBasis.Xb = Vector3( Xb );
+        rtnBasis.Yb = Vector3( Yb );
+        rtnBasis.Zb = Vector3( Zb );
+        rtnBasis.Pt = Vector3( Pt );
+        return rtnBasis;
+    }
+};
+typedef shared_ptr<Basis> basPtr; // 2023-06-17: For now assume that Bases are lightweight enough to pass by value
+
+
+
+////////// TOYS ////////////////////////////////////////////////////////////////////////////////////
+
+class DynaMesh{ public:
+    // Straightforward container for a `Mesh` with changing geometry
+    // 2023-08-14: This class assumes that the mesh will have a constant size for each buffer, though their values might change
+    // 2023-08-15: This class REQUIRES that vertices, indices, normals, and colors ALL be defined
+
+    /// Members ///
+    uint /*------*/ Ntri;
+    uint /*------*/ Nvtx;
+    vector<triPnts> tris; // Dynamic geometry, each array is a facet of 3x vertices
+    vector<triPnts> nrms; // Normal vector for each facet
+    vector<triClrs> clrs; // Vertex colors for each facet
+    Matrix /*----*/ xfrm; // Pose in the parent frame
+    Mesh /*------*/ mesh; // Raylib mesh geometry
+    bool /*------*/ upldMesh; // Has the mesh been uploaded?
+    Model /*-----*/ modl; // Model
+    bool /*------*/ upldModl; // Has the mesh been uploaded?
+    Shader /*----*/ shdr; // Shader
+    
+    /// Memory Methods ///
+
+    void init_mesh_memory( uint Ntri_ ){
+        // Create the `Mesh` struct and allocate memory there
+
+        Ntri = Ntri_;
+        Nvtx = Ntri*3;
+
+        // 1. Init mesh
+        upldMesh = false;
+        upldModl = false;
+        mesh     = Mesh{};
+
+        mesh.triangleCount = Ntri;
+        mesh.vertexCount   = Nvtx;
+        
+        cout << "GO!: init_mesh_memory( " << mesh.triangleCount << " )" << endl;
+
+        // 2. Init memory
+        mesh.vertices = (float* ) MemAlloc(Nvtx*3 * sizeof( float  )); // 3 vertices, 3 coordinates each (x, y, z)
+        mesh.indices  = (ushort*) MemAlloc(Nvtx   * sizeof( ushort ));
+        mesh.normals  = (float* ) MemAlloc(Nvtx*3 * sizeof( float  )); // 3 vertices, 3 coordinates each (x, y, z)
+        mesh.colors   = (u_char*) MemAlloc(Nvtx*4 * sizeof( u_char )); // 3 vertices, 4 coordinates each (r, g, b, a)
+    }
+
+    void load_mesh_buffers( bool loadGeo = true, bool loadColor = false ){
+        // Load geometry (and color) info into `mesh` buffers        
+        // 2023-08-14: For now assume that the facet indices do NOT change!
+
+        // 0. Init
+        ulong k = 0; // Vertex _ counter
+        ulong l = 0; // Index __ counter
+        ulong m = 0; // Color __ counter
+
+        // 1. Load CPU-side
+        for( uint i = 0; i < Ntri; ++i ){
+            for( u_char j = 0; j < 3; j++ ){
+                if( loadGeo ){
+                    mesh.normals[k]  = nrms[i][j].x;
+                    mesh.vertices[k] = tris[i][j].x;  k++;
+                    mesh.normals[k]  = nrms[i][j].y;
+                    mesh.vertices[k] = tris[i][j].y;  k++;
+                    mesh.normals[k]  = nrms[i][j].z;
+                    mesh.vertices[k] = tris[i][j].z;  k++;
+                    mesh.indices[l]  = l; /*-------*/ l++; 
+                }
+                if( loadColor ){
+                    mesh.colors[m] = clrs[i][j].r;  m++;
+                    mesh.colors[m] = clrs[i][j].g;  m++;
+                    mesh.colors[m] = clrs[i][j].b;  m++;
+                    mesh.colors[m] = clrs[i][j].a;  m++;
+                }
+            }
+        }
+
+        // If this mesh is not present on the GPU, then send it
+        if( !upldMesh ){
+            // 3. Initial load to GPU
+
+            cout << "Initial `Mesh` upload, ID: " << mesh.vaoId << endl;
+
+            // UploadMesh( mesh, true );
+            UploadMesh( &mesh, true );
+
+            upldMesh = true;
+        // Else mesh is on GPU, update the mesh buffers there
+        }else{
+            // 2. Send GPU-side
+            if( loadGeo ){
+                UpdateMeshBuffer( 
+                    mesh, // -------------------------------- `Mesh` object
+                    VERTEX_BUFFER_IDX, // ------------------- VBO Index
+                    mesh.vertices, // ----------------------- Array of data 
+                    mesh.vertexCount*3 * sizeof( float  ), // Total array size
+                    0 // ------------------------------------ Starting index in array
+                );
+                UpdateMeshBuffer( 
+                    mesh, // -------------------------------- `Mesh` object
+                    NORMAL_BUFFER_IDX, // ------------------- VBO Index
+                    mesh.normals, // ------------------------ Array of data 
+                    mesh.vertexCount*3 * sizeof( float  ), // Total array size
+                    0 // ------------------------------------ Starting index in array
+                );
+                // 2023-08-14: For now assume that the facet indices do NOT change!
+            }
+            if( loadColor ){
+                UpdateMeshBuffer( 
+                    mesh, // -------------------------------- `Mesh` object
+                    COLORS_BUFFER_IDX, // ------------------- VBO Index
+                    mesh.colors, // ------------------------- Array of data 
+                    mesh.vertexCount*4 * sizeof( u_char ), // Total array size
+                    0 // ------------------------------------ Starting index in array
+                );
+            }
+        }
+    }
+
+    void remodel(){
+        cout << "About to create model ... " << flush;
+        modl = LoadModelFromMesh( mesh );
+        modl.materials[0] = LoadMaterialDefault();
+        modl.materials[0].shader = shdr;
+        upldModl = true;
+        cout << "Model created!" << endl;
+    }
+
+    ~DynaMesh(){
+        cout << "." << flush;
+    }
+
+    /// Constructors ///
+
+    DynaMesh( uint Ntri ){
+        // Allocate memory and set default pose
+        init_mesh_memory( Ntri );
+        xfrm = MatrixIdentity();
+    }
+
+    /// Geometry Methods ///
+
+    void push_triangle_w_norms( triPnts tri ){
+        // Add one triangle
+        Vector3 norm = normal_of_tiangle( tri );  
+        tris.push_back( tri );
+        nrms.push_back({ norm, norm, norm });
+    }
+    
+    void wipe_geo( bool wipeTris = true, bool wipeColors = false ){
+        // Remove all built geometry
+        if( wipeTris ){
+            tris.clear();
+            nrms.clear();
+        }
+        if( wipeColors ){
+            clrs.clear();
+        }
+    }
+
+    /// Pose Math ///
+    /* xfrm = 
+    m0 m4 m8  m12
+    m1 m5 m9  m13
+    m2 m6 m10 m14
+    m3 m7 m11 m15
+    */
+
+    Vector3 get_posn(){
+        // Get the position components of the homogeneous coordinates as a vector
+        return Vector3{ xfrm.m12, xfrm.m13, xfrm.m14 };
+    }
+
+    void set_posn( const Vector3& posn ){
+        // Set the position components of the homogeneous coordinates
+        xfrm.m12 = posn.x;
+        xfrm.m13 = posn.y;
+        xfrm.m14 = posn.z;
+    }
+
+    void translate( const Vector3& delta ){
+        // Increment the position components of the homogeneous coordinates by the associated `delta` components
+        xfrm.m12 += delta.x;
+        xfrm.m13 += delta.y;
+        xfrm.m14 += delta.z;
+    }
+
+    void rotate_RPY( float r_, float p_, float y_ ){
+		// Increment the world Roll, Pitch, Yaw of the model
+        xfrm = MatrixMultiply( 
+            MatrixMultiply( MatrixMultiply( MatrixRotateY( y_ ), MatrixRotateX( p_ ) ), MatrixRotateZ( r_ ) ), 
+            xfrm 
+        );
+	}
+
+    ///// Drawing Context Methods ////////////////
+    // NOTE: Method beyond this point require a drawing context being instantiated before calling!
+
+    /// Rendering ///
+
+    void set_shader( Shader shader ){ shdr = shader; }
+
+    void draw(){
+        // Render the mesh
+        if( !upldModl )  remodel();
+        modl.transform = xfrm;
+        DrawModel( modl, get_posn(), 1.0f, WHITE );
+
+    }
+
+};
+
+#endif /* RL_TOYBOX_H */ 
---
+++ C++/Raylib/utils.hpp
@@ -0,0 +1,45 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+////////// INIT ////////////////////////////////////////////////////////////////////////////////////
+
+///// Imports ////////////////////////////////////
+
+/// Standard ///
+#include <stdlib.h>  // srand, rand
+#include <time.h>
+#include <iostream>
+using std::cout, std::endl, std::flush;
+
+///// Aliases ////////////////////////////////////
+typedef unsigned char ubyte;
+
+
+////////// RANDOM NUMBERS //////////////////////////////////////////////////////////////////////////
+
+float randf(){
+    // Return a pseudo-random number between 0.0 and 1.0
+    return  1.0f * rand() / RAND_MAX;
+}
+
+float randf( float lo, float hi ){
+    // Return a pseudo-random number between `lo` and `hi`
+    // NOTE: This function assumes `hi > lo`
+    float span = hi - lo;
+    return lo + span * randf();
+}
+
+int randi( int lo, int hi ){
+    // Return a pseudo-random number between `lo` and `hi` (int)
+    int span = hi - lo;
+    return lo + (rand() % span);
+}
+
+ubyte rand_ubyte(){
+    // Return a pseudo-random unsigned byte
+    return (ubyte) randf( 0.0, 256.0 );
+}
+
+void rand_seed(){  srand( time(NULL) );  } // Seed RNG with unpredictable time-based seed
+
+#endif /* UTILS_H */
