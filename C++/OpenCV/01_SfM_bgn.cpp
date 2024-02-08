bf2c67661c321afd740d52281e8fba9c3cbd640e
James Watson <james.watson-2@colorado.edu>
2024-02-08 11:19:29 -0700

    Brute force mutual keypoint matching, Next: Relative pose estimation

--- C++/OpenCV/01_SfM_bgn.cpp
+++ C++/OpenCV/01_SfM_bgn.cpp
@@ -1,4 +1,4 @@
-// g++ 01_SfM_bgn.cpp `pkg-config --cflags --libs opencv4` -std=c++17 -lopencv_xfeatures2d -I /usr/local/include/opencv4/ -o load-img.out
+// g++ 01_SfM_bgn.cpp `pkg-config --cflags --libs opencv4` -std=c++17 -lopencv_xfeatures2d -I /usr/local/include/opencv4/ -o load-kp.out
 
 ////////// DEV PLAN & NOTES ////////////////////////////////////////////////////////////////////////
 /*
@@ -14,12 +14,17 @@
     [Y] Compute ORB for all images, 2024-02-02: Easy!
     [>] Feature matching
         [>] Match features between each pair of images in the input image set,
-            https://docs.opencv.org/3.4/d5/d6f/tutorial_feature_flann_matcher.html
-            [>] Brute force
+            [Y] Brute force, 2024-02-08: Not the longest wait ever
             [ ] Approximate nearest neighbour library, such as FLANN, ANN, Nanoflann
-        [ ] Bi-directional verification
-    [ ] Relative pose estimation
+            https://docs.opencv.org/3.4/d5/d6f/tutorial_feature_flann_matcher.html
+        [>] Bi-directional verification
+            [Y] Brute force, 2024-02-08: Have to wait quite a while to get mutual matches from brute force,
+                5321 matched keypoint pairs for two images with 50000 features each.
+            [ ] Approximate nearest neighbour
+    [>] Relative pose estimation
         [ ] For each pair of images with sufficient number of matches, compute relative pose
+            * 2024-02-08: For now, assume that pictures with consecutive alpha filenames are related by virtue
+                          of being taken in burst fashion while orbiting the subject. Close the loop. No search needed.
         [ ] N-view triangulation
         [ ] Bundle adjustment
     [ ] Two-view SfM
@@ -70,7 +75,7 @@ using std::shared_ptr;
 #include <map>
 using std::pair;
 #include <cmath>
-using std::abs;
+using std::abs, std::min;
 
 /// Special ///
 #include <opencv2/opencv.hpp>
@@ -94,7 +99,7 @@ using cv::ORB, cv::normL2Sqr;
 
 ////////// UTILITY FUNCTIONS ///////////////////////////////////////////////////////////////////////
 
-vector<string> list_files_at_path( string path ){
+vector<string> list_files_at_path( string path, bool sortAlpha = true ){
     // List all the files found at a path
     vector<string> rtnNams;
     string /*---*/ path_i;
@@ -102,6 +107,7 @@ vector<string> list_files_at_path( string path ){
         path_i = entry.path().string();
         rtnNams.push_back( path_i );  
     }
+    if( sortAlpha )  std::sort( rtnNams.begin(),rtnNams.end() );
     return rtnNams;
 }
 
@@ -170,10 +176,17 @@ class CamShot{ public:
 };
 typedef shared_ptr<CamShot> shotPtr;
 
+float keypoint_dist_sqr( const KeyPoint& a, const KeyPoint& b ){
+    // Return the squared L2 norm between two points in pixels squared
+    return pow(a.pt.x - b.pt.x, 2) + pow(a.pt.y - b.pt.y, 2);
+}
+
 float keypoint_diff_mag( const KeyPoint& a, const KeyPoint& b, float areaFactor = 50.0f, float angleFactor = 50.0f/M_PI ){
     // Calculate keypoint difference as the angle diff scaled by fraction of overlapping area
     //     <Fraction of non-overlapping area>*<constant> + <Angle difference>*<constant>
-    return (1.0f - KeyPoint::overlap( a, b ))*areaFactor + abs( a.angle - b.angle )*angleFactor;
+    return keypoint_dist_sqr( a, b ) + 
+           (1.0f - KeyPoint::overlap( a, b ))*areaFactor + 
+           abs( a.angle - b.angle )*angleFactor;
 }
 
 size_t N_KM = 0;
@@ -186,11 +199,19 @@ struct KpMatch{
     float  diff;
 
     KpMatch( size_t prevDex_, size_t nextDex_, float diff_ ){
-        ++N_KM;
+        // Assign a unique ID and set values
         ID = N_KM;
         prevDex = prevDex_;
         nextDex = nextDex_;
         diff    = diff_;
+        ++N_KM;
+    }
+
+    bool p_same_indices( const KpMatch& other ){
+        // Return true if both matches contain the same indices
+        return ( ((prevDex == other.prevDex) && (nextDex == other.nextDex))
+                 ||
+                 ((prevDex == other.nextDex) && (nextDex == other.prevDex)) );
     }
 };
 
@@ -213,7 +234,7 @@ class ShotPair{ public:
 
     /// Methods ///
 
-    void brute_force_match(){
+    void brute_force_match( ulong topK ){
         // Linear search for best matches, O(n^2)
         KeyPoint /*--*/ kp_p, kp_n;
         size_t /*----*/ iM = 0, jM = 0;
@@ -221,6 +242,8 @@ class ShotPair{ public:
         size_t /*----*/ Nn = next->kpts.size();
         float /*-----*/ dMin, d;
         size_t /*----*/ div = 1000;
+        vector<KpMatch> PtoN;
+        vector<KpMatch> NtoP;
 
         // TBD: Eliminate one-to-many matches?
         // vector<KpMatch> candidates;
@@ -241,21 +264,70 @@ class ShotPair{ public:
                     jM   = jn;
                 }
             }
-            matches.push_back( KpMatch{iM,jM,dMin} );
+            PtoN.push_back( KpMatch{iM,jM,dMin} );
             if( (ip%div) == 0 ) cout << "." << flush;
         }
-        cout << endl;
+        cout << " Complete! Find reverse matches " << flush;
+
+        for( size_t jn = 0; jn < Nn; ++jn ){
+            dMin = 1e9;
+            kp_n = next->kpts[jn];
+            for( size_t ip = 0; ip < Np; ++ip ){
+                kp_p = prev->kpts[ip];    
+                d    = keypoint_diff_mag( kp_p, kp_n );
+                if( d < dMin ){
+                    dMin = d;
+                    iM   = ip;
+                    jM   = jn;
+                }
+            }
+            NtoP.push_back( KpMatch{iM,jM,dMin} );
+            if( (jn%div) == 0 ) cout << "." << flush;
+        }
+        cout << " Complete! Filter for mutual matches " << flush;
+
+        std::sort( PtoN.begin(), PtoN.end(), [](KpMatch &a, KpMatch &b){ return a.diff < b.diff; });
+        std::sort( NtoP.begin(), NtoP.end(), [](KpMatch &a, KpMatch &b){ return a.diff < b.diff; });
+
+        size_t K = min( min( topK, PtoN.size() ), NtoP.size() );
+        
+        /* To make the matching results more robust, a bi-directional verification is performed, which requires that 
+           matches in each direction need to be among the top K matching points of one another. */
+        for( size_t i = 0; i < K; ++i ){
+            for( size_t j = 0; j < K; ++j ){
+                if( PtoN[i].p_same_indices( NtoP[j] ) ){
+                    matches.push_back( PtoN[i] );
+                    break;
+                }
+                if( (j%div) == 0 ) cout << "." << flush;
+            }
+        }
+        cout << " Complete!" << endl;
+        
+            
         
         // 2024-02-07: For now, allow one-to-many matching and assume the best matches will win ...
+    }
+
+    void report_matches( size_t limit = 0 ){
+        // Print out matches and their closeness
+        cout << "There are " << matches.size() << " matched keypoint pairs." << endl;
+        size_t i = 0;
+        for( KpMatch& match : matches ){ 
+            cout << "\t" << match.ID << ": " << match.prevDex << "<->" << match.nextDex << " = " << match.diff << endl;
+            ++i;
+            if( (i>0) && (i>=limit) ) break;
+        }
+        cout << endl;
     }
 };
 
 
-vector<shotPtr> shots_from_images( const vector<string>& paths, const vector<Mat>& images ){
+vector<shotPtr> shots_from_images( const vector<string>& paths, const vector<Mat>& images, ulong Nfeat = 10000 ){
     // Construct a camera shot object for every photo in the vector
     vector<shotPtr> /*----*/ rtnShots;
     cout << "About to get keypoints ... " << flush;
-    vector<vector<KeyPoint>> kps = calc_ORB_keypoints_from_images( images );
+    vector<vector<KeyPoint>> kps = calc_ORB_keypoints_from_images( images, Nfeat );
     cout << "Obtained!" << endl;
     size_t /*-------------*/ N   = images.size();
     for( size_t i = 0; i < N; ++i ){
@@ -267,15 +339,22 @@ vector<shotPtr> shots_from_images( const vector<string>& paths, const vector<Mat
 ////////// MAIN ////////////////////////////////////////////////////////////////////////////////////
 int main(){
 
+    ulong Nfeat = 50000;
+
     vector<string> fNames = list_files_at_path( _IMG_PATH );
     for( string fName : fNames ) cout << fName << endl;
     cout << endl;
-    vector<Mat> images = fetch_images_at_path( _IMG_PATH );
+    vector<Mat> images = fetch_images_at_path( _IMG_PATH, 2 );
     cout << endl;
-    vector<shotPtr> shots = shots_from_images( fNames, images );
+    vector<shotPtr> shots = shots_from_images( fNames, images, Nfeat );
     cout << endl;
     for( shotPtr& shot : shots ){
         cout << shot->path << ", KP: " << shot->kpts.size() << endl;
     }
+
+    ShotPair sp{ shots[0], shots[1] };
+    sp.brute_force_match( (ulong)(Nfeat/2) );
+    sp.report_matches( 100 );
+
     return 0;
 }
