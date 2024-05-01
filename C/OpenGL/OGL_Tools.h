ab82ddee352f6062d66dacdfdb495b8a0d28ac74
James Watson <james.watson-2@colorado.edu>
2024-05-01 13:48:36 -0600

    LUNCHTIME

---
+++ C/OpenGL/OGL_Tools.h
@@ -0,0 +1,7 @@
+// 2024-05-01: This is a revised version of "OGL_Utils.h" and is meant to replace it in the future.
+
+#ifndef OGL_TOOLS_H // This pattern is to prevent symbols to be loaded multiple times
+#define OGL_TOOLS_H // from multiple imports
+
+
+#endif
