5ad3704cddf836da47cf17a4d80eaded74ae78a3
James Watson <james.watson-2@colorado.edu>
2024-12-11 09:47:09 -0700

    PLEASE STOP

--- C++/OpenCV/00_Asteroid.cpp
+++ C++/OpenCV/00_Asteroid.cpp
@@ -1,7 +1,7 @@
 // g++ 00_Asteroid.cpp SfM.cpp image_proc.cpp helpers.cpp  `pkg-config --cflags --libs opencv4` -std=c++17 -lopencv_xfeatures2d -I /usr/local/include/opencv4/ -o build-asteroid.out
 
-// #include <matio.h>
 #include <matioCpp/matioCpp.h>
+using matioCpp::Element;
 
 #include "SfM.hpp"
 
@@ -19,11 +19,44 @@ const float  foc_length  = cam_res/(2*Tanf(fov_deg/2)); // [pixels]
 const Mat    cam_intr    = camera_instrinsics( foc_length, foc_length, cam_res/2,cam_res/2 );
 const ubyte  n_poses     = baselinesFile.read("baselines").dimensions()[1];
 
+// NOTE: MATLAB IS COLUMN-MAJOR
+
+Mat copy_slice_from_MatIO_3d( matioCpp::MultiDimensionalArray<double> matioMat, uint rows, uint cols, uint depth, uint sliceDex ){
+    Mat     rtnMat = Mat::zeros( rows, cols, CV_32F );
+    double* arr    = matioMat.data();
+    Element<double> elem;
+
+    // FIXME, START HERE: SEGMENTATION FAULT, GET THE SIZE OF THE ARRAY
+
+    for( uint i = 0; i < rows; ++i ){
+        for( uint j = 0; j < cols; ++j ){
+            /* build-asteroid.out: /usr/local/include/matioCpp/impl/MultiDimensionalArray.tpp:333: 
+            matioCpp::MultiDimensionalArray<T>::element_type& matioCpp::MultiDimensionalArray<T>::operator[](matioCpp::MultiDimensionalArray<T>::index_type) 
+            [with T = double; matioCpp::MultiDimensionalArray<T>::reference = double&; matioCpp::MultiDimensionalArray<T>::index_type = long unsigned int]: 
+            Assertion `el < numberOfElements() && "[matioCpp::MultiDimensionalArray::operator[]] 
+            The required element is out of bounds."' failed.
+            Aborted (core dumped)
+            */
+            rtnMat.at<float>( j, i ) = (float) arr[ i*cols*depth + j*depth + sliceDex ];
+        }
+    }
+    return rtnMat;
+}
 
 int main( int argc, char* argv[] ){
 
+    Mat /*---*/ rot_prev;
+    Mat /*---*/ rot_curr;
+    Mat /*---*/ rel_pos = Mat::zeros( 3, n_poses-1, CV_32F );
+    vector<Mat> rel_rot_cam;
+    for( ubyte i = 0; i < (n_poses-1); ++i ){  rel_rot_cam.push_back(  Mat::zeros(3, 3, CV_32F)  );  }
     
     
+
+    for( ubyte i = 0; i < n_poses; ++i ){
+        rot_prev = copy_slice_from_MatIO_3d( rot_cam2ast, 3, 3, n_poses, i   );
+        rot_curr = copy_slice_from_MatIO_3d( rot_cam2ast, 3, 3, n_poses, i-1 );
+    }
 
     cout << rot_cam2astFile.read("rot_cam2ast").name() << ", " << endl
          << rot_cam2astFile.read("rot_cam2ast").dimensions()[0] << ", " 
