c30ea4473e14a8107a75892b4890d1a4a4385a2e
James Watson <james.watson-2@colorado.edu>
2025-01-30 16:42:14 -0700

    Not right now

---
+++ C++/OpenCV/10_Sculpture/SfM.hpp
@@ -0,0 +1,38 @@
+////////// INIT ////////////////////////////////////////////////////////////////////////////////////
+
+///// Includes /////
+
+/// Standard ///
+#include <vector>
+using std::vector;
+#include <string>
+using std::string, std::to_string, std::stof;
+#include <memory>
+using std::shared_ptr;
+
+/// Special ///
+#include <opencv2/opencv.hpp>
+#include "opencv2/core.hpp"
+#include <opencv2/core/utility.hpp>
+using cv::Mat, cv::String, cv::Vec;
+#include <opencv2/imgcodecs.hpp>
+using cv::imread, cv::IMREAD_COLOR, cv::IMREAD_GRAYSCALE;
+#include "opencv2/features2d.hpp"
+using cv::Ptr, cv::KeyPoint, cv::Point2f, cv::FeatureDetector;
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////// Structure.cpp ///////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class ImgNode{ public:
+    // Represents an image and all of the information inferred from that image
+    
+    /// Members ///
+    string /*-----*/ path;
+    Mat /*--------*/ imag;
+    vector<KeyPoint> kpts;
+    Mat /*--------*/ xfrm;
+};
+typedef shared_ptr<ImgNode> nodePtr;
