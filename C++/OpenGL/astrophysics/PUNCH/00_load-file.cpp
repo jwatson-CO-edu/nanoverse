37afa6e1ecfee93f9bbdf56741cc56fce8b45bbf
James Watson <james.watson-2@colorado.edu>
2025-07-17 07:48:12 -0600

    smol work

--- C++/OpenGL/astrophysics/PUNCH/00_load-file.cpp
+++ C++/OpenGL/astrophysics/PUNCH/00_load-file.cpp
@@ -2,21 +2,60 @@
 using std::string;
 #include <vector>
 using std::vector;
+#include <array>
+using std::array;
+#include <map>
+using std::map;
 #include <iostream>
 using std::cout, std::endl;
 #include <fitsio.h>
 
 typedef vector<string> vstr;
+typedef array<long,2>  addr;
+
+void load_arr( addr coords, long* arr ){
+    for( size_t i = 0; i < coords.size(); ++i ){  arr[i] = coords[i];  }
+}
 
 class FITS_File { public:
-    fitsfile* fptr;
+    /// Data ///
+    fitsfile* /*-----------*/ fptr    = nullptr;
+    void* /*---------------*/ dataArr = nullptr;
     vector<char[ FLEN_CARD ]> cards;
-    int /*-*/ status; 
-    int /*-*/ nkeys;
-    string    path;
-    vstr /**/ keys;
-    int /*-*/ datatype;
-    int /*-*/ Naxes;
+    int /*-----------------*/ status; 
+    int /*-----------------*/ nkeys;
+    string /*--------------*/ path;
+    vstr /*----------------*/ keys;
+    int /*-----------------*/ datatype;
+    int /*-----------------*/ Naxes;
+    /// Null Values ///
+    map<int,void*>   nullPxlVal;
+    unsigned char    TBYTE_NULL     = 0;     
+    signed char /**/ TSBYTE_NULL    = 0;    
+    signed short     TSHORT_NULL    = 0;    
+    unsigned short   TUSHORT_NULL   = 0;   
+    signed int /*-*/ TINT_NULL /**/ = 0;      
+    unsigned int     TUINT_NULL     = 0;     
+    signed long /**/ TLONG_NULL     = 0;     
+    signed long long TLONGLONG_NULL = 0; 
+    unsigned long    TULONG_NULL    = 0;    
+    float /*------*/ TFLOAT_NULL    = 0.0f;    
+    double /*-----*/ TDOUBLE_NULL   = 0.0;  
+    
+
+    void setup_null_pixel_values(){         
+        nullPxlVal[ TBYTE     ] = &TBYTE_NULL;
+        nullPxlVal[ TSBYTE    ] = &TSBYTE_NULL;
+        nullPxlVal[ TSHORT    ] = &TSHORT_NULL;
+        nullPxlVal[ TUSHORT   ] = &TUSHORT_NULL;
+        nullPxlVal[ TINT      ] = &TINT_NULL;
+        nullPxlVal[ TUINT     ] = &TUINT_NULL;
+        nullPxlVal[ TLONG     ] = &TLONG_NULL;
+        nullPxlVal[ TLONGLONG ] = &TLONGLONG_NULL;
+        nullPxlVal[ TULONG    ] = &TULONG_NULL;
+        nullPxlVal[ TFLOAT    ] = &TFLOAT_NULL;
+        nullPxlVal[ TDOUBLE   ] = &TDOUBLE_NULL;
+    }
     
     void report_status( string prefix = "" ){  
         /* print any error messages */
@@ -41,6 +80,7 @@ class FITS_File { public:
     void load_keys(){
         for( int ii = 1; ii <= nkeys; ii++ ){ 
             fits_read_record( fptr, ii, cards[ii-1], &status ); /* read keyword */
+            keys[ii-1] = string( cards[ii-1] );
             printf( "key %i: %s\n", ii-1, cards[ii-1] );
         }
     }
@@ -48,6 +88,15 @@ class FITS_File { public:
     void close(){
         fits_close_file( fptr, &status );
         report_status( "FILE CLOSE" );
+    }
+
+    void fetch_pixel_data( addr coords, long nelements ){
+        long pxlAdr[2];
+        load_arr( coords, pxlAdr );
+        fits_read_pix( fptr, datatype, pxlAdr, 
+                       nelements, nullPxlVal[ datatype ], 
+                       void *array, 
+                       int *anynul, int *status)
     }
 };
 
