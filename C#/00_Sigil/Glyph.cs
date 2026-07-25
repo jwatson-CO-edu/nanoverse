2d8ad77fb4c8758e09da3f6ec5b6fce2cc473669
James Watson <james.watson-2@colorado.edu>
2026-07-25 11:53:27 -0600

    Tangent & Curvature of Quad & Cubic

--- C#/00_Sigil/Glyph.cs
+++ C#/00_Sigil/Glyph.cs
@@ -1,10 +1,55 @@
+using OpenTK.Mathematics;
 using spline;
 
 namespace sigil {
 
-public class Stroke ( Parametric param ) {
-    Parametric curve = param;
+
+public readonly struct Tri{
+    private readonly Vector3[] verts = new Vector3[3]; // Array ref cannot change, values can 
+
+    public Tri( Vector3 a, Vector3 b, Vector3 c ){
+        verts[0] = a;
+        verts[1] = b;
+        verts[2] = c;
+    }
+
+    public readonly Vector3 this[int index]{
+        get => verts[ index ];
+        set => verts[ index ] = value;
+    }
+
+    public readonly Vector3 V0() => verts[0];
+    public readonly Vector3 V1() => verts[1];
+    public readonly Vector3 V2() => verts[2];
 }
+
+
+
+public class Stroke {
+    public const int  _DEFAULT_DIV = 32;
+    public float /**/ thick;
+    public Parametric curve;
+    public int /*--*/ div;
+    public List<Tri>  geo;
+
+    public Stroke(){
+        thick = 0.0f;
+        curve = new DummyCurve();
+        div   = _DEFAULT_DIV;
+        geo   = [];
+    }
+
+    public Stroke( Parametric param, float thickness, int div_ = _DEFAULT_DIV ){
+        thick = thickness;
+        curve = param;
+        div   = div_;
+        geo   = [];
+        geo.Capacity = 2*div;
+    }
+}
+
+
+
 
 
 }
--- C#/00_Sigil/Spline.cs
+++ C#/00_Sigil/Spline.cs
@@ -6,7 +6,16 @@ namespace spline {
 /// Abstract base class for all Parametric curves 
 /// </summary>
 public abstract class Parametric {
-    public abstract Vector3 Val( float t );
+    public abstract Vector3 Val( float t ); // Value
+    public abstract Vector3 Tan( float t ); // Tangent
+    public abstract Vector3 Crv( float t ); // Curvature
+}
+
+
+public class DummyCurve : Parametric {
+    public override Vector3 Val( float t ){  return new Vector3();  }
+    public override Vector3 Tan( float t ){  return new Vector3();  }
+    public override Vector3 Crv( float t ){  return new Vector3();  }
 }
 
 
@@ -21,19 +30,54 @@ public class Bezier {
     /// </summary>
     public class Quad ( Vector3 P0, Vector3 P1, Vector3 P2 ) : Parametric {
 
-        public static Vector3 Value( Vector3 P0, Vector3 P1, Vector3 P2, float t  ){
+        /// <summary>
+        /// Point on the curve for parameter `t`
+        /// </summary>
+        public static Vector3 Value( Vector3 P0, Vector3 P1, Vector3 P2, float t ){
             float oneMinusT = 1.0f - t;
             float omtSquard = oneMinusT * oneMinusT;
             float tSquared  = t * t;
             return omtSquard*P0 + 2*oneMinusT*t*P1 + tSquared*P2;
         }
 
+        
+        /// <summary>
+        /// Tangent on the curve for parameter `t`
+        /// </summary>
+        public static Vector3 Tangent( Vector3 P0, Vector3 P1, Vector3 P2, float t ){
+            float oneMinusT = 1.0f - t;
+            return 2*oneMinusT*(P1 - P0) + 2*t*(P2 - P1);
+        }
+
+
+        /// <summary>
+        /// Curvature at parameter `t`
+        /// </summary>
+        public static Vector3 Curvature( Vector3 P0, Vector3 P1, Vector3 P2 ){
+            return 2*(P2 - 2*P1 + P0);
+        }
+
         public Vector3 p0 = P0;
         public Vector3 p1 = P1;
         public Vector3 p2 = P2; 
 
-        public override Vector3 Val( float t ){  return Value( p0, p1, p2, t );  }
-
+        
+        /// <summary>
+        /// Point on the curve for parameter `t`
+        /// </summary>
+        public override Vector3 Val( float t ){  return Value( p0, p1, p2, t );    }
+        
+        
+        /// <summary>
+        /// Tangent on the curve for parameter `t`
+        /// </summary>
+        public override Vector3 Tan( float t ){  return Tangent( p0, p1, p2, t );  }
+        
+        
+        /// <summary>
+        /// Curvature at parameter `t`
+        /// </summary>
+        public override Vector3 Crv( float t ){  return Curvature( p0, p1, p2 );   }
     }
 
 
@@ -42,6 +86,9 @@ public class Bezier {
     /// </summary>
     public class Cubic ( Vector3 P0, Vector3 P1, Vector3 P2, Vector3 P3 ) : Parametric {
 
+        /// <summary>
+        /// Point on the curve for parameter `t`
+        /// </summary>
         public static Vector3 Value( Vector3 P0, Vector3 P1, Vector3 P2, Vector3 P3, float t ){
             float oneMinusT = 1.0f - t;
             float omtSquard = oneMinusT * oneMinusT;
@@ -51,17 +98,49 @@ public class Bezier {
             return omtCubed*P0 + 3.0f*omtSquard*t*P1 + 3*oneMinusT*tSquared*P2 + tCubed*P3;
         }
 
+        
+        /// <summary>
+        /// Tangent on the curve for parameter `t`
+        /// </summary>
+        public static Vector3 Tangent( Vector3 P0, Vector3 P1, Vector3 P2, Vector3 P3, float t ){
+            float oneMinusT = 1.0f - t;
+            float omtSquard = oneMinusT * oneMinusT;
+            float tSquared  = t * t;
+            return 3*omtSquard*(P1 - P0) + 6*oneMinusT*t*(P2 - P1) + 3*tSquared*(P3 - P2);
+        }
+
+        
+        /// <summary>
+        /// Curvature at parameter `t`
+        /// </summary>
+        public static Vector3 Curvature( Vector3 P0, Vector3 P1, Vector3 P2, Vector3 P3, float t ){
+            float oneMinusT = 1.0f - t;
+            return 6*oneMinusT*(P2 - 2*P1 + P0) + 6*t*(P3 - 2*P2 + P1);
+        }
+
         Vector3 p0 = P0;
         Vector3 p1 = P1;
         Vector3 p2 = P2; 
         Vector3 p3 = P3;
 
 
-        public override Vector3 Val( float t ){  return Value( p0, p1, p2, p3, t );  }
-
-
+        /// <summary>
+        /// Point on the curve for parameter `t`
+        /// </summary>
+        public override Vector3 Val( float t ){  return Value( p0, p1, p2, p3, t );      }
+        
+        
+        /// <summary>
+        /// Tangent on the curve for parameter `t`
+        /// </summary>
+        public override Vector3 Tan( float t ){  return Tangent( p0, p1, p2, p3, t );    }
+        
+        
+        /// <summary>
+        /// Curvature at parameter `t`
+        /// </summary>
+        public override Vector3 Crv( float t ){  return Curvature( p0, p1, p2, p3, t );  }
     }
-
 }
 
 
