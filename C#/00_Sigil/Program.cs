c87c3fcb7cc43d6edd78b6147418ef10c6a795c5
James Watson <james.watson-2@colorado.edu>
2026-08-06 13:00:19 -0600

    Detailed build instructions to that the Tea Leaves can find it easily?

--- C#/00_Sigil/Program.cs
+++ C#/00_Sigil/Program.cs
@@ -3,12 +3,20 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine( "Hello, World!" );
 
+string dir = "";
+
+if( args.Length > 0 ){
+    Console.WriteLine( $"First argument: {args[0]}" );
+    dir = args[0];
+}else{
+    Console.WriteLine( "No arguments passed." );
+}
+
 Pictogram sgl = new();
 
 while( sgl.strokes.Count == 0 ){
     sgl.Generate();
     sgl.FilterSmall( 0.25f );
-    // sgl.FilterFar( 1.0f );
     sgl.FilterFar( 0.75f );
 }
 
@@ -17,8 +25,14 @@ while( sgl.strokes.Count == 0 ){
 
 // sgl.ShiftToCentroid();
 sgl.ShiftToCenter();
+string fileName;
+if( dir.Length > 0 ){
+    fileName = $"{dir}/sigil_{Guid.NewGuid():N}.jpg";
+}else{
+    fileName = $"sigil_{Guid.NewGuid():N}.jpg";
+}
 
-SigilWindow rndr = new( sgl.GetAllTriangles(), "test.jpg" );
+SigilWindow rndr = new( sgl.GetAllTriangles(), fileName );
 rndr.Run();
 
-Console.WriteLine( "TASK COMPLETE!\n" );
+Console.WriteLine( $"Created: {fileName}\n" );
---
+++ C#/00_Sigil/build.md
@@ -0,0 +1,1 @@
+$> `dotnet publish -c Release -r linux-x64 --self-contained false /p:DebugType=None /p:PublishSingleFile=true /p:AssemblyName="Sigil.out" -o ~/Project_Stargazer/python/`
