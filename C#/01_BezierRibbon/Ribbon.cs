fb78fdfd8237fbe6af07a703ca7801553f1f7dcd
James Watson <james.watson-2@colorado.edu>
2026-08-07 14:33:36 -0600

    Find the segment???

--- C#/01_BezierRibbon/Ribbon.cs
+++ C#/01_BezierRibbon/Ribbon.cs
@@ -12,27 +12,50 @@ public class Constants {
 /// </summary>
 public class Ribbon {
     
-    public int /*-----*/ div; // - Number of chunks that define the ribbon
-    public float /*---*/ twist; // Radians of twist along `t` = [0,1]  
-    public Parametric    spine; // Curve at the center of the ribbon
-    public float /*---*/ dt; // -- Parametric length of each chunk
-    public List<float>   t; // --- Parameter of chunk borders
-    public List<float>   Xwdt; //- Width along X at chunk borders
-    public List<Vector3> Xdir; //- Width direction at chunk borders
+    public int /*-----*/ div; // -- Number of chunks that define the ribbon
+    public int /*-----*/ N; // ---- Number of boundaries that define the ribbon
+    public float /*---*/ twist; //- Radians of twist along `t` = [0,1]
+    public float /*---*/ dTheta; // Twist along each chunk  
+    public Parametric    spine; //- Curve at the center of the ribbon
+    public float /*---*/ dt; // --- Parametric length of each chunk
+    public List<float>   tLst; // ---- Parameter of chunk borders
+    public List<float>   Xwdt; // - Width along X at chunk borders
+    public List<Vector3> Xdir; // - Width direction at chunk borders
     
     
+    /// <summary>
+    /// Default Constructor 
+    /// </summary>
     public Ribbon ( int div_ = Constants._DEFAULT_DIV, float twist_ = 0f ){
-        div   = div_;
-        twist = twist_;
-        dt    = 1.0f / div;
-        spine = new DummyCurve();
-        t     = [];
-        Xwdt  = [];
-        Xdir  = [];
-        Xdir.Capacity = div_+1;
-        t.Capacity    = div_+1;
-        Xwdt.Capacity = div_+1;
-        for( int i = 0; i <= div; ++i ){  t.Add( i * dt ); }
+        div    = div_;
+        N /**/ = div_+1;
+        twist  = twist_;
+        dt     = 1.0f / div;
+        dTheta = twist / div;
+        spine  = new DummyCurve();
+        tLst   = [];
+        Xwdt   = [];
+        Xdir   = [];
+        Xdir.Capacity = N;
+        tLst.Capacity    = N;
+        Xwdt.Capacity = N;
+        for( int i = 0; i <= div; ++i ){  tLst.Add( i * dt ); }
+    }
+
+
+    public int GetSegment( float t ){
+        for( int i = 0; i < N; ++i ){  if( tLst[i] > t ){  return i-1;  }  }
+        return N-1;
+    }
+
+
+    public void SetXdirAt( Vector3 xDir, float t = 0f ){
+        int     i    = GetSegment(t);
+        Vector3 Zdir = spine.Tan(t).Normalized();
+        Vector3 Ydir = Vector3.Cross( Zdir, xDir ).Normalized();
+        if( i > 0 ){
+
+        }
     }
     
 
