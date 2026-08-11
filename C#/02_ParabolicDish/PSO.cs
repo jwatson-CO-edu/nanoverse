3a74f188b119cc6405916e362e20250ad3358d6d
James Watson <james.watson-2@colorado.edu>
2026-08-11 10:55:20 -0600

    Less Reflection, Less crazy?

--- C#/02_ParabolicDish/PSO.cs
+++ C#/02_ParabolicDish/PSO.cs
@@ -32,23 +32,51 @@ foreach (var kvp in data)
 /// </summary>
 public class PSOptimizer<T> {
 
-    public List<string> /*---------*/ fields    = [];
-    public Dictionary<string,float[]> ranges    = [];
-    public LinkedList<T> /*--------*/ particles = [];
-    public T? /*-------------------*/ bestPrtcl = default;
+    public List<string> /*--------------------*/ fields    = [];
+    public Dictionary<string,float[]> /*------*/ ranges    = [];
+    public LinkedList<Dictionary<string,float>>  particles = [];
+    public Dictionary<string,float> /*--------*/ bestPrtcl = [];
+    public Dictionary<string,float> /*--------*/ particle  = [];
+    public Func<Dictionary<string,float>,float>? Score     = null;
+    public Random /*--------------------------*/ rand /**/ = new();
 
     
-    public void AddFields( params string[] fieldsArr ){
-        fields.AddRange( fieldsArr );
+    /// <summary>
+    /// Add a list of fields
+    /// </summary>
+    public void AddFields( params string[] fieldsArr ){  fields.AddRange( fieldsArr );  }
+
+
+    /// <summary>
+    /// Set range [lo,hi,step,] for one field
+    /// </summary>
+    public void AddDomainDiv( string field, float lo, float hi, int div = 10 ){
+        if( fields.Contains( field ) ){
+            if( lo > hi ){  throw new Exception( $"Domain [{lo}, {hi}] is MALFORMED!" );  }
+            ranges[ field ] = [lo, hi, div, (hi-lo)/div,];
+        }
+        throw new Exception( $"Key {field} DNE for this problem!" );
     }
 
 
-    public void AddRange( string field, float[] range ){
-        if( fields.Contains( field ) ){
-            if( range.Length < 2 ){ throw new Exception( $"Expected two bounds, Got {range.Length}!" ); }
-            ranges[ field ] = range;
+    /// <summary>
+    /// Set function that will be used for scoring each particle
+    /// </summary>
+    public void SetScoringFunc( Func<Dictionary<string,float>,float>? func ){  Score = func;  }
+
+
+    public Dictionary<string,float> RandParticle(){
+        Dictionary<string,float> rtnPrt = [];
+        foreach( string field in fields ){
+            rtnPrt[ field ] = ranges[ field ][0] + rand.Next( (int) ranges[ field ][2] + 1 ) * ranges[ field ][3];
         }
-        throw new Exception( $"Key {field} DNE for this problem!" );
+        rtnPrt[ "score" ] = float.NaN;
+        return rtnPrt;
+    }
+
+
+    public void PopulateInit( int N = 1000 ){
+        for( int i = 0; i < N; ++i ){  particles.AddLast( RandParticle() );  }
     }
 
 
