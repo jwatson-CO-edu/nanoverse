650d4f4db92ec2013a3f2889bcc091d378f4741d
James Watson <james.watson-2@colorado.edu>
2026-08-13 09:52:27 -0600

    DEV_PLAN

--- C#/02_ParabolicDish/Program.cs
+++ C#/02_ParabolicDish/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/* ////////// DEV_PLAN /////////////////////////////////////////////////////////////////////////////
+
+[ ] Run a dish solution (Test PSO) 
+[ ] Construct Discretized Dish
+[ ] Display dish w Phong Shading
+[ ] Allow user to rotate dish
+[ ] Lay out discretized units onto standard size rectangles in OpenGL
+[ ] Confirm SVG instructions for laser cutting
+[ ] Lay out discretized units onto standard size rectangles in SVG
+[ ] Design ribs
+[ ] Lay out ribs onto standard size rectangles in SVG
+
+*/
+
+
+////////// MAIN ////////////////////////////////////////////////////////////////////////////////////
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
