cac86eb981c62c6a6e051e7d141837b2bb8b6006
James Watson <james.watson-2@colorado.edu>
2026-08-19 09:51:33 -0600

    POWERFUL shapes!

--- C#/03_ShipGenerator/Ideogram.cs
+++ C#/03_ShipGenerator/Ideogram.cs
@@ -8,18 +8,20 @@ public class MeshGen {
     public Random rand = new();
 
 
-    public static List<Tri> Cube( Vector3 center, float side = 1f ){
+    public static List<Tri> Cuboid( Vector3 center, float Xside = 1f, float Yside = 1f, float Zside = 1f ){
         List<Tri> rtnShp = [];
-        float     sHalf  = side / 2f;
+        float     xHalf  = Xside / 2f;
+        float     yHalf  = Yside / 2f;
+        float     zHalf  = Zside / 2f;
         rtnShp.Capacity = 12;
-        Vector3 p0 = center - Vector3.UnitX * sHalf - Vector3.UnitY * sHalf - Vector3.UnitZ * sHalf;
-        Vector3 p1 = center - Vector3.UnitX * sHalf + Vector3.UnitY * sHalf - Vector3.UnitZ * sHalf;
-        Vector3 p2 = center + Vector3.UnitX * sHalf + Vector3.UnitY * sHalf - Vector3.UnitZ * sHalf;
-        Vector3 p3 = center + Vector3.UnitX * sHalf - Vector3.UnitY * sHalf - Vector3.UnitZ * sHalf;
-        Vector3 p4 = center - Vector3.UnitX * sHalf - Vector3.UnitY * sHalf + Vector3.UnitZ * sHalf;
-        Vector3 p5 = center + Vector3.UnitX * sHalf - Vector3.UnitY * sHalf + Vector3.UnitZ * sHalf;
-        Vector3 p6 = center + Vector3.UnitX * sHalf + Vector3.UnitY * sHalf + Vector3.UnitZ * sHalf;
-        Vector3 p7 = center - Vector3.UnitX * sHalf + Vector3.UnitY * sHalf + Vector3.UnitZ * sHalf;
+        Vector3 p0 = center - Vector3.UnitX * xHalf - Vector3.UnitY * yHalf - Vector3.UnitZ * zHalf;
+        Vector3 p1 = center - Vector3.UnitX * xHalf + Vector3.UnitY * yHalf - Vector3.UnitZ * zHalf;
+        Vector3 p2 = center + Vector3.UnitX * xHalf + Vector3.UnitY * yHalf - Vector3.UnitZ * zHalf;
+        Vector3 p3 = center + Vector3.UnitX * xHalf - Vector3.UnitY * yHalf - Vector3.UnitZ * zHalf;
+        Vector3 p4 = center - Vector3.UnitX * xHalf - Vector3.UnitY * yHalf + Vector3.UnitZ * zHalf;
+        Vector3 p5 = center + Vector3.UnitX * xHalf - Vector3.UnitY * yHalf + Vector3.UnitZ * zHalf;
+        Vector3 p6 = center + Vector3.UnitX * xHalf + Vector3.UnitY * yHalf + Vector3.UnitZ * zHalf;
+        Vector3 p7 = center - Vector3.UnitX * xHalf + Vector3.UnitY * yHalf + Vector3.UnitZ * zHalf;
         
         /*   6 ---- 5
            / |    / |
@@ -54,6 +56,75 @@ public class MeshGen {
 
         return rtnShp;
     }
+
+
+    public static List<Tri> TrapezoidSlab( Vector3 center, float thickness = 1f,
+                                           float xSideTop    = 1f, float ySideTop    = 1f, 
+                                           float xSideBottom = 1f, float ySideBottom = 1f, 
+                                           int xPlaceTop    = 0, int yPlaceTop    = 0,
+                                           int xPlaceBottom = 0, int yPlaceBottom = 0 ){
+        List<Tri> rtnShp = [];
+        rtnShp.Capacity = 12;
+        
+        float xHalfTop  = xSideTop / 2f;
+        float yHalfTop  = ySideTop / 2f;
+        float xHalfBtm  = xSideBottom / 2f;
+        float yHalfBtm  = ySideBottom / 2f;
+        float xShiftTop = Math.Max( xHalfBtm - xHalfTop, 0f ); // Calculate shift for the smaller X
+        float yShiftTop = Math.Max( yHalfBtm - yHalfTop, 0f ); // Calculate shift for the smaller Y
+        float xShiftBtm = Math.Max( xHalfTop - xHalfBtm, 0f ); // Calculate shift for the smaller X
+        float yShiftBtm = Math.Max( yHalfTop - yHalfBtm, 0f ); // Calculate shift for the smaller Y
+        float zHalf     = thickness / 2f;
+
+        Vector3 top = center + Vector3.UnitX * xShiftTop * xPlaceTop    + Vector3.UnitY * yShiftTop * yPlaceTop    + Vector3.UnitZ * zHalf;
+        Vector3 btm = center + Vector3.UnitX * xShiftBtm * xPlaceBottom + Vector3.UnitY * yShiftBtm * yPlaceBottom - Vector3.UnitZ * zHalf;
+
+        Vector3 p0 = top - Vector3.UnitX * xHalfTop - Vector3.UnitY * yHalfTop;
+        Vector3 p1 = top - Vector3.UnitX * xHalfTop + Vector3.UnitY * yHalfTop;
+        Vector3 p2 = top + Vector3.UnitX * xHalfTop + Vector3.UnitY * yHalfTop;
+        Vector3 p3 = top + Vector3.UnitX * xHalfTop - Vector3.UnitY * yHalfTop;
+
+        Vector3 p4 = btm - Vector3.UnitX * xHalfBtm - Vector3.UnitY * yHalfBtm;
+        Vector3 p5 = btm + Vector3.UnitX * xHalfBtm - Vector3.UnitY * yHalfBtm;
+        Vector3 p6 = btm + Vector3.UnitX * xHalfBtm + Vector3.UnitY * yHalfBtm;
+        Vector3 p7 = btm - Vector3.UnitX * xHalfBtm + Vector3.UnitY * yHalfBtm;
+        
+        /*   6 ----- 5
+           /  \    /   \
+         7 ----- 4       \
+         |     2 -\------- 3
+         |   /     \     /
+         | /        \  /
+         1 --------- 0   */
+        
+        // Bottom //
+        rtnShp.Add( new Tri( p0, p1, p2 ) );
+        rtnShp.Add( new Tri( p2, p3, p0 ) );
+
+        // Top //
+        rtnShp.Add( new Tri( p4, p5, p6 ) );
+        rtnShp.Add( new Tri( p6, p7, p4 ) );
+
+        // Front //
+        rtnShp.Add( new Tri( p0, p4, p7 ) );
+        rtnShp.Add( new Tri( p7, p1, p0 ) );
+
+        // Back //
+        rtnShp.Add( new Tri( p2, p6, p5 ) );
+        rtnShp.Add( new Tri( p5, p3, p2 ) );
+
+        // Right //
+        rtnShp.Add( new Tri( p4, p0, p3 ) );
+        rtnShp.Add( new Tri( p3, p5, p4 ) );
+        
+        // Left //
+        rtnShp.Add( new Tri( p1, p7, p6 ) );
+        rtnShp.Add( new Tri( p6, p2, p1 ) );
+
+
+        return rtnShp;
+
+    } 
 
 
     public List<Tri> Cylinder( Vector3 center, Vector3 axis, float radius = 1f, float height = 1f, int arcDiv = 16, int hgtDiv = 2 ){
