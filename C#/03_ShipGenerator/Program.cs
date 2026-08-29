a5ef1ca5dc35d3c9e8713999a488d57e5a7a1832
James Watson <james.watson-2@colorado.edu>
2026-08-29 07:01:22 -0600

    Render Cuboid

--- C#/03_ShipGenerator/Program.cs
+++ C#/03_ShipGenerator/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using geo3d;
+using ideogram;
+using OpenTK.Mathematics;
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+MeshGen mg = new();
+
+List<Tri> cuboid = MeshGen.Cuboid( new Vector3(0,0,0), 1f, 2f, 3f );
+cuboid = Tri.ColorMesh( cuboid, new Vector4(1,0,0,1) );
+
+TriMeshViewer.Show( cuboid );
+
+
+Console.WriteLine( "Program COMPLETE!" );
