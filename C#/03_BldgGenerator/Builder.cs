b6216e7c98b6caeb388d336a29804372849b6ec5
James Watson <james.watson-2@colorado.edu>
2026-09-01 18:08:04 -0600

    Stacked apartment building

--- C#/03_BldgGenerator/Builder.cs
+++ C#/03_BldgGenerator/Builder.cs
@@ -4,15 +4,32 @@ using geo3d;
 
 namespace ideogram {
 
+/// <summary>
+/// Builder pattern for building meshes
+/// </summary>
 public static class Builder {
 
-    public static List<Tri> StackedApartment( Vector3 centerBase, int Nunit, 
+    public static List<Tri> StackedApartment( Vector3 centerBase, Vector3 color, int Nunit, 
                                               float wideWidth, float wideHeight, float narrowWidth, float narrowHeight ){
         List<Tri> totMsh = [];
+        Vector3   base_i = centerBase;
+        List<Tri> mesh_i;
 
-        // MeshGen.Cuboid(  )
+        for( int i = 0; i < Nunit; ++i ){
 
-        return totMsh;
+            base_i[3] += narrowHeight/2;
+            mesh_i = MeshGen.Cuboid( base_i, narrowWidth, narrowWidth, narrowHeight );
+            totMsh.AddRange( mesh_i );
+
+            base_i[3] += narrowHeight/2 + wideHeight/2;
+            mesh_i = MeshGen.Cuboid( base_i, wideWidth, wideWidth, wideHeight );
+            totMsh.AddRange( mesh_i );
+
+            base_i[3] += wideHeight/2;
+            
+        }
+
+        return Tri.ColorMesh( totMsh, new Vector4( color, 1f ) ) ;
     }
 
 }
