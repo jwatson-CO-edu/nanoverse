d45b01ae15106d8cb96180c53daf26a4ab9b04e4
James Watson <james.watson-2@colorado.edu>
2026-09-03 21:44:10 -0600

    tune size

--- C#/01_BezierRibbon/Program.cs
+++ C#/01_BezierRibbon/Program.cs
@@ -3,7 +3,7 @@ using ribbon;
 
 Console.WriteLine( "Hello, World!" );
 
-GlyphGen gg = new();
+GlyphGen gg = new( NgridHalf : 3 );
 List<Tri> totMesh = gg.MakeGlyph();
 Console.WriteLine( $"{totMesh.Count} triangles to draw!" );
 
