4976be51d352ad799d5f04db9f31d09315812427
James Watson <james.watson-2@colorado.edu>
2026-09-05 10:56:15 -0600

    brainstorming latin glyphs

--- C#/01_BezierRibbon/Glyph.cs
+++ C#/01_BezierRibbon/Glyph.cs
@@ -7,6 +7,9 @@ namespace ribbon;
 
 
 /* ////////// DEV_PLAN /////////////////////////////////////////////////////////////////////////////
+* Parent Class
+    [ ] "Parent" Parameter `Val`, Meta-`t`
+    [ ] Meta-`Tan`
 [ ] Parametric Arc
 [ ] Ribbon Arc
 
@@ -14,29 +17,112 @@ namespace ribbon;
 */
 
 
+/// <summary>
+/// Component of a Latin-style glyph
+/// </summary>
 public class LatinStroke : IMeshHaver {
     
-    /// Creation ///
+    ///// Members /////
     public RNode /*-------*/ bgn /**/ = new();
     public RNode /*-------*/ end /**/ = new();
     public RNode /*-------*/ center   = new();
-    public Vector3 /*-----*/ mainAxis = Vector3.UnitY;
+    public Vector3 /*-----*/ mainAxis = Vector3.Zero;
     public List<LatinStroke> edges    = [];
-    public Ribbon /*------*/ stroke   = new();
+    public List<Ribbon> /**/ strokes  = [];
 
-    /// Connection ///
+
+    ///// Methods /////
+    
+    /// Connection Methods ///
+    public bool HasAxis(){  return mainAxis.Length > MathVec3._EPSILON;  }
     public void AddEdge( LatinStroke neighbor ){  edges.Add( neighbor );  }
+    public virtual int Occupancy(){  return edges.Count;  }
 
-    /// Mesh ///
-    public List<Tri> GetTotalMesh(){  return stroke.GetTotalMesh();  }
+
+    /// Mesh Methods ///
+    public List<Tri> GetTotalMesh(){  
+        List<Tri> rtnMsh = [];
+        foreach( Ribbon stroke in strokes ){  rtnMsh.AddRange( stroke.GetTotalMesh() );  }
+        return rtnMsh;  
+    }
+
+}
+
+
+
+// QWERTYUIOPASDFGHJKLZXCVBNM
+// qwertyuiopasdfghjklzxcvbnm
+
+
+/// <summary>
+/// Container for Latin glyph fragments
+/// </summary>
+public static class Latin {
+
+
+    /// <summary>
+    /// Any closed ellipse in a glyph
+    /// </summary>
+    public class Ellipse : LatinStroke {
+
+    }
+
+
+    /// <summary>
+    /// Any elliptical bend in a glyph
+    /// </summary>
+    public class ArcEllp : LatinStroke {
+
+    }
+
+
+    /// <summary>
+    /// Any straight structure in a glyph
+    /// </summary>
+    public class Spar : LatinStroke {
+
+    }
+
+
+    /// <summary>
+    /// Quadratic Bezier
+    /// </summary>
+    public class QBez : LatinStroke {
+
+    }
+
+
+    /// <summary>
+    /// Cubic Bezier
+    /// </summary>
+    public class CBez : LatinStroke {
+
+    }
+
+
+    /// <summary>
+    /// Fancy terminator that prevents further connection
+    /// </summary>
+    public class Serif : LatinStroke {
+        public override int Occupancy(){  return int.MaxValue;  }
+    }
+
+
+    /// <summary>
+    /// Floating fragments that should not be connected to
+    /// </summary>
+    public class Kipple : LatinStroke {
+        public override int Occupancy(){  return int.MaxValue;  }
+    }
+
 
 }
 
 
 /// <summary>
-/// Generate a glyph with Latin components and patterns
+/// Generate a glyph with Latin components and patterns, Build rules here
 /// </summary>
-public class LatinGlyph {
+public class LatinGlyph { // : IMeshHaver 
 
 
 
