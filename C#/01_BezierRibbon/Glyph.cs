cc201a99a0f53bc3a58b2c2f742403859970e690
James Watson <james.watson-2@colorado.edu>
2026-09-06 17:08:43 -0600

    Line

--- C#/01_BezierRibbon/Glyph.cs
+++ C#/01_BezierRibbon/Glyph.cs
@@ -1,7 +1,7 @@
 using OpenTK.Mathematics;
 
 using geo3d;
-
+using parametric;
 
 namespace ribbon;
 
@@ -111,7 +111,45 @@ public static class Latin {
     /// Any elliptical bend in a glyph
     /// </summary>
     public class ArcEllp : LatinStroke {
+        public Vector3 cntr;
+        public Vector3 norm;
+        public Vector3 begin;
+        public float   scale;
+        public float   aScl;
+        public float   bScl;
+        public float   bgnTheta;
+        public float   endTheta;
 
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public ArcEllp ( Vector3 cntr_, Vector3 norm_, Vector3 begin_, float scale_ = 0f, float a = 0f, float b = 0f, 
+                         float bgnTheta_ = 0f, float endTheta_ = 0f ){
+            cntr     = cntr_;
+            norm     = norm_;
+            begin    = begin_;
+            scale    = scale_;
+            bgnTheta = bgnTheta_;
+            endTheta = endTheta_;
+            if( scale_ > MathVec3._EPSILON ){
+                aScl = aFctr * scale_;
+                bScl = bFctr * scale_;
+            }else{
+                aScl = a;                    
+                bScl = b;                        
+            }
+            
+            Ribbon rbbn = new(){
+                spine = new parametric.Ellipse.OvalSection( cntr, norm, begin, aScl, bScl, bgnTheta, endTheta )
+            };
+            strokes.Add( rbbn );
+
+            center   = new(){  posn = cntr  };
+            mainAxis = ( parametric.Ellipse.Oval.Value( cntr, norm, begin, aScl, bScl, 0.25f ) -
+                         parametric.Ellipse.Oval.Value( cntr, norm, begin, aScl, bScl, 0.75f ) ).Normalized();
+            bgn = new(){  posn = strokes[0].spine.Val( 0f )  };
+            end = new(){  posn = strokes[0].spine.Val( 1f )  };
+        }
     }
 
 
@@ -120,6 +158,24 @@ public static class Latin {
     /// </summary>
     public class Spar : LatinStroke {
 
+        public Vector3 linBgn;
+        public Vector3 linEnd;
+
+        public Spar( Vector3 linBgn_, Vector3 linEnd_ ){
+
+            linBgn = linBgn_;                    
+            linEnd = linEnd_; 
+
+            Ribbon rbbn = new(){
+                spine = new Line.Segment( linBgn, linEnd )
+            };
+            strokes.Add( rbbn );
+
+            bgn /**/ = new(){  posn = linBgn  };
+            end /**/ = new(){  posn = linEnd  };
+            center   = new(){  posn = (linBgn + linEnd)/2f  };
+            mainAxis = (linEnd - linBgn).Normalized(); 
+        }
     }
 
 
